9ed45422ffb3bb643db3416bdb49d645a725f604
gabrielmirandat <gabrielmirandatt@gmail.com>
2017-04-24 11:15:22 -0300

    Added mrpt project for bayes program

---
+++ Programs/undergraduate/robotics_topics/probabilistic/bayes_program/mrpt/mrpt.cc
@@ -0,0 +1,19 @@
+//  // Robot pose: 2D (x,y,phi)
+//  CPose2D R (2 ,1 , DEG2RAD (45.0) );
+
+//  // Camera pose relative to the robot : 6 D (x ,y ,z , yaw , pitch , roll ).
+//  CPose3D C ( 0.5 ,0.5 ,1.5 , DEG2RAD ( -90.0) , DEG2RAD (0) , DEG2RAD ( -90.0) );
+
+//  cout << " R : " << R << endl ;
+//  cout << " C : " << C << endl ;
+//  cout << " R + C : " << ( R + C ) << endl ;
+//  cout << " |R - C |= " << R . distanceTo ( C ) << endl ;
+
+#include <mrpt/utils/CProbabilityDensityFunction.h>
+
+int main()
+{
+
+
+    return 0;
+}
