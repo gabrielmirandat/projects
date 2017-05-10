feb8123cd5682c8f3beb086ba9d59d434330ba59
Gabriel Miranda <gabrielmirandatt@gmail.com>
2017-05-10 20:13:18 -0300

    Finished study for nd test

--- undergraduate/law_notions/nd.cc
+++ undergraduate/law_notions/nd.cc
@@ -30,27 +30,146 @@ Direito e sua função - norma e conduta juridica
 	surge declaração dos direitos do Homem e do cidadão, direitos Humanos, ...
 		
 Norma jurídica - forma prescritiva (aquele que fala pretende direcionar o caminho do outro)
-		- são ordens, mandados, imposições (induzir comportamento)
-		- superioridade do emissor sobre o destinatário
-		- é um dever ser (não faz sentido dizer que é verdadeira ou falsa, mas eficiente ou não)
-		- são os ditos princípios
-			- conjunto de normas que espelham a ideologia da constituição, seus postulados básicos e seus fins
-			- principios em conflito não se invalidam, escolhe-se o que melhor funciona no caso concreto
-			- é o balanceamento por peso de principios
-		- regras ou são válidas ou invalidas, sendos os princípios superiores às regras
-			- regras que colidem são corrigidas com regras mais específicas, senão é inválida 
+	- são ordens, mandados, imposições (induzir comportamento)
+	- superioridade do emissor sobre o destinatário
+	- é um dever ser (não faz sentido dizer que é verdadeira ou falsa, mas eficiente ou não)
+	- são os ditos princípios
+		- conjunto de normas que espelham a ideologia da constituição, seus postulados básicos e seus fins
+		- principios em conflito não se invalidam, escolhe-se o que melhor funciona no caso concreto
+		- é o balanceamento por peso de principios
+	- regras ou são válidas ou invalidas, sendos os princípios superiores às regras
+		- regras que colidem são corrigidas com regras mais específicas, senão é inválida 
+
+	- caso fácil
+		- interprete é mero aplicador da norma
+	- caso difícil
+		- resolvível com principios e no caso balanceamento
+
+	- dilema constitucional
+		- eleição entre bens distintos ou perda fundamental de um bem
+		- é o conflito entre direitos fundamentais excludentes, mais grave que o caso difícil
+		- caso Evans, direito de não ser pai contra direito da mãe ser mãe 
+		  com seus óvulos congelados fertilizados com os espermatozoides do pai
+		- caso das irmãs siameses em que apenas uma sobreviveria
+		- judia que deve escolher um dos filhos para viver nos campos nazistas
+		- usa-se a ponderação no caso de colisão parcial entre direitos fundamentais
 			
-		- caso fácil
-			- interprete é mero aplicador da norma
-		- caso difícil
-			- resolvível com principios e no caso balanceamento
+Ordenamento jurídico - conjunto de todas as normas existentes em determinado Estado. A teoria se dá:
+	- conceber hierarquia normativa
+		- pirâmede normativa
+		- perspectiva de validade
+		- A constituição nas leis produzidas pelo poder legislativo
+		- Estas validam decisões proferidas pelos juízes e dos decretos via administração
+		- A validade na norma inferior está condicionada à norma superior
+		- norma que explica unidade do ordenamento é a norma fundamental
+	- deve oferecer critérios de resolução de conflitos
+		- resolvida por critério hierárquico, critério da especificidade, critério cronológico
+	- discutir lacunas do direito
+		- a lacuna está na lei, mas não no ordenamento
+		- para suprir lacuna, integração via fontes normativas
+
+	- fontes normativas
+		- Lei: principal fonte do direito (nos países civil law)
+			- é o ato legislativo (Legislativo), não o ato administrativo (Executivo) ou 
+			  jurisprudência (Judiciário)
+			- a lei limita, e por limitar liberta
+			- é o texto normativo acordado pelos órgaos legislativo
+			- a Constituição assegura a existência de princípios fundamentais de forma que 
+			  nenhuma lei pode contrariar
+		- Costumes: norma aceita como obrigatória pela consciência do povo, sem que o poder público 
+			    a tenha estabelecido
+			- pode ser aplicado pelo poder judiciário
+		- Jurisprudência: decisões judiciais reiteradas
+			- dão interpretação e concretização no caso concreto
+		- Doutrina: todo conhecimento jurídico para ensinar a teoria do direito
 			
-		- dilema constitucional
-			- eleição entre bens distintos ou perda fundamental de um bem
-			- é o conflito entre direitos fundamentais, mais grave que o caso difícil
-			- caso Evans, direito de não ser pai contra direito da mãe ser mãe 
-			  com seus óvulos congelados fertilizados com os espermatozoides do pai
-			- caso das irmãs siameses em que apenas uma sobreviveria
-			- judia que deve escolher um dos filhos para viver nos campos nazistas
+Teoria da justiça
+	- Justiça é um conceito fundamental da filosofia, ética, religião, direito
+	- Para o direito, a justiça é um ideal messiânico
+	- Toda reflexão crítica das normas jurídicas envolve uma concepção de justiça
+	- tensões: mudança contra estabilidade, individual contra o bem comum, liberdade contra igualdade
+	- Toda pessoa está limitada pela linguagem
+	- A formação das ideias, além da inteligência e imaginação são controladas pela linguagem
+	
+	- John Ralws
+		- justiça é a primeira virtude das instutuições sociais 
+		- pretende elaborar teoria da justiça que seja viável 
+		- rivais são o intuicionismo e utilitarismo
+		- intuicionismo - não é viável escolher um principio dentre tantos para resolver um conflito
+				- o que vai nesse caso é a intuição
+		- utilitarismo - considera ato como correto quando maximiza a felicidade geral
+		- contratualismo de Ralws
+			- moral exige que cumpramos obrigações que nos comprometemos a cumprir
+			- veu da ignorancia que impede sujeitos de conhecer sua classe e status social,
+  			  inteligencia, força, raça, qualquer informação que lhes permita decidir em favor próprio
+			- os sujeitos têm motivação de obter bens primários, de cunho social - riqueza, oportunidades e 
+			  e direitos, de cunho natural - talentos, saúde, inteligência
+			- cada um terá direito igual de obtenção das liberdades básicas
+			- vantagens se dão no tempo, empregos e cargos acessíveis a todos
+			- indivíduos responsáveis por suas ações
+			- a natureza não é justa ou injusta, as instituições são no modo como processa os fatos da natureza
 			
-			11.3.1 - pag 24
+	- Nozick - concepção libertária
+		- um Estado mínimo dedicado exclusivamente a proteger as pessoas contra roubo, fraude, uso ilegítimo 
+		  de força e amparar o cumprimento dos contratos celebrados entre esses indivíduos
+		- cada um é o legítimo proprietário de seu corpo
+		- Estado só assegura liberdade negativa
+		- Estado não deve fornecer nada aos indivíduos para que possam levar adiante seus planos de vida
+		- O fato de você ser forçado a contribuir para o bem-estar de outro viola seus direitos
+		- A liberdade quebra qualquer norma igualitária
+		- Quem chega primeiro aproveita primeiro
+		
+	- Dworkin - concepção igualitária
+		- igualar as pessoas em suas circunstâncias
+		- disponibilizar recursos iguais às pessoas
+		- princípio da tolerância
+	
+	- Amaruja Sen
+		- a igualdade buscada deve ser analisada na capacidade de cada sujeito
+		  para converter ou transformar esses recursos em liberdades
+		- o foco é a liberdade, não conquistas específicas
+		
+	- Feminista
+		- Enfoque da dominação
+		
+Marxismo Analítico
+	- As instituições podem ser explicadas em torno do indivíduo: ações, propriedade e relações
+	- Diferente do marxismo clássico
+	- Trabalhadores versus necessitados
+	- Renda básica universal e socialismo de heroado
+	
+Comunitarismo de Hegel
+	- Prioridade a nossos laços comunitários
+	- Prevalência de virtudes como fraternidade ou solidariedade
+	- Liberdade positiva com cidadania ativa
+	
+Republicanismo
+	- Prioridade de direitos coletivos
+	- Relevância das tradições
+	
+Democracia
+	- Participação do cidadão no governo, diretamente ou por representantes
+	- Sufrágio universal e secreto, em eleições períodicas e legítimas
+	- Mandato político versus mandato civil
+	- Sistema partidário: pluripartidarismo ou bipartidarismo
+	- Eleições: majoritária ou proporcional
+	- Democracia deliberativa
+		- ações governamentais devem ser conhecidas pela coletividade
+		- possibilidade de responsabilização dos governantes
+			
+			
+		
+	
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
