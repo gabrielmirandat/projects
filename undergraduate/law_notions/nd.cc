393275b3808a91e7c48552c62ffd2333773033bc
Gabriel Miranda <gabrielmirandatt@gmail.com>
2017-05-10 10:54:05 -0300

    Updated general studying for nd test

--- undergraduate/law_notions/nd.cc
+++ undergraduate/law_notions/nd.cc
@@ -6,3 +6,51 @@ Fatos jurídicos 'lato sensu' podem ser divididos em:
 	- Atos jurídicos 'lato sensu' (comportamentos humanos);
 		- Atos jurídicos 'stricto sensu' (efeitos derivados da lei);
 		- negócios jurídicos (efeitos queridos pelas partes);
+
+//----------------------------------------------------
+Direito - conjunto de normas para regular o comportamento humano em sociedade
+	Jusnatularista - direito natural do homem, por ordem de coisas natural
+			- ordem cosmica, Deus, antropocentrismo, vinculado à moral
+	Juspositivista - nada transcendente, do convivio humano, ordenamento juridico
+			- Kelsen, ciencias, fatos sociais, sem correção moral
+	Pós-positivista - adição de princípios (aproximação entre moral e direito)
+			- pós segunda guerra
+			- valores colhidos pela socidade
+			
+Direito e sua função - norma e conduta juridica
+	Norma - regula a conduta humana
+	Conduta juridica - norma determinada pela conduta humana
+	
+	Enquanto limitador do poder politico - direito vem sendo construido pela tensao 
+	entre ideal de justiça e realidade da violencia na qual se ampara o poder, ou seja, 
+	esta submissao de um sujeito a outro
+	
+	Com a separação de poderes o Estado se alto limita
+	Liberadade pública, defesa do individuo contra abusos do Estado, que era o Leviatã (monstro biblico)
+	surge declaração dos direitos do Homem e do cidadão, direitos Humanos, ...
+		
+Norma jurídica - forma prescritiva (aquele que fala pretende direcionar o caminho do outro)
+		- são ordens, mandados, imposições (induzir comportamento)
+		- superioridade do emissor sobre o destinatário
+		- é um dever ser (não faz sentido dizer que é verdadeira ou falsa, mas eficiente ou não)
+		- são os ditos princípios
+			- conjunto de normas que espelham a ideologia da constituição, seus postulados básicos e seus fins
+			- principios em conflito não se invalidam, escolhe-se o que melhor funciona no caso concreto
+			- é o balanceamento por peso de principios
+		- regras ou são válidas ou invalidas, sendos os princípios superiores às regras
+			- regras que colidem são corrigidas com regras mais específicas, senão é inválida 
+			
+		- caso fácil
+			- interprete é mero aplicador da norma
+		- caso difícil
+			- resolvível com principios e no caso balanceamento
+			
+		- dilema constitucional
+			- eleição entre bens distintos ou perda fundamental de um bem
+			- é o conflito entre direitos fundamentais, mais grave que o caso difícil
+			- caso Evans, direito de não ser pai contra direito da mãe ser mãe 
+			  com seus óvulos congelados fertilizados com os espermatozoides do pai
+			- caso das irmãs siameses em que apenas uma sobreviveria
+			- judia que deve escolher um dos filhos para viver nos campos nazistas
+			
+			11.3.1 - pag 24
