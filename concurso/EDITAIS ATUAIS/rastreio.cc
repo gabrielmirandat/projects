4abb588968d712f18ef9a378339bc35279ed855c
gabrielmirandat <gabrielmirandatt@gmail.com>
2017-05-13 14:11:39 -0300

    Nova estrutura e primeiro set de provas baixados.

---
+++ concurso/EDITAIS ATUAIS/rastreio.cc
@@ -0,0 +1,19 @@
+senado
+  - confirmado
+  - edital no segundo semestre de 2017
+
+camara
+  - confirmado
+  - edital proximo
+
+receita
+  - previsto
+  - 2017
+
+bacen
+  - previsto
+  - 2017/2018
+
+tcu
+  - previsto
+  - 2017/2018 
