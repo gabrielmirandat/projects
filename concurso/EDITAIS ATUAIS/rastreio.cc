085e4c7d610444456dbe20a9b60baa48baa38f21
gabrielmirandat <gabrielmirandatt@gmail.com>
2017-06-01 20:34:38 -0300

    Atualizado rastreio.

--- concurso/EDITAIS ATUAIS/rastreio.cc
+++ concurso/EDITAIS ATUAIS/rastreio.cc
@@ -1,19 +1,41 @@
-senado
+senado  (estudar prova)
   - confirmado
   - edital no segundo semestre de 2017
 
-camara
+camara  (estudar prova)
   - confirmado
   - edital proximo
 
-receita
+receita  (estudar prova)
   - previsto
   - 2017
 
-bacen
+bacen  (estudar prova)
   - previsto
-  - 2017/2018
+  - 2017/2018  (estudar prova)
 
-tcu
+tcu  (estudar prova)
   - previsto
   - 2017/2018 
+
+
+
+
+
+
+
+
+
+
+receita
+1. Língua Portuguesa
+2. Espanhol ou Inglês
+3. Raciocínio Lógico-Quantitativo
+4. Administração Geral e Pública
+5. Direito Constitucional
+6. Direito Administrativo
+7. Direito Tributário
+8. Auditoria
+9. Contabilidade Geral e Avançada
+10. Legislação Tributária e Comércio Internacional
+11. Legislação Aduaneira
