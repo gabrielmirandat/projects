58c7ccd059af18965b71371f6ef833f320db966d
gabrielmirandat <gabrielmirandatt@gmail.com>
2017-06-01 20:37:59 -0300

    Adicionada primeira estrategia para estudar para concurso

---
+++ concurso/estrategia.cc
@@ -0,0 +1,16 @@
+
+ESTRATEGIA 1 (começando a estudar pra concurso..)
+	ETAPA 1
+		Pega uma prova
+		Fazer toda a prova
+		Checar resultado (anotar resultados para rastreio)
+	FIM ETAPA1
+
+	ETAPA 2
+		Passar pela prova
+			anotar assunto de forma geral (QConcursos) e se preciso específica (com tag única para facilitar rastreio em fontes extensas)
+			estudar o assunto anotado
+			explicar porque a questao está certa ou errada
+		Fim prova
+	FIM ETAPA2
+FIM ESTRATEGIA
