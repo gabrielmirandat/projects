5ae21420e90fc55f6307ad8a55e33d7c32330a09
gabrielmirandat <gabrielmirandatt@gmail.com>
2017-06-04 15:37:23 -0300

    Added study for SO test 3

--- undergraduate/operational_systems/tutorials/so.cc
+++ undergraduate/operational_systems/tutorials/so.cc
@@ -1,3 +1,4 @@
+//PROVA 2
 //\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\//
 unix.pdf
 
@@ -473,6 +474,84 @@ dados de entrada de uma thread produzidos pela thread anterior
 	Não há chamadas de sistema bloqueadas
 	#programação completa
 	#permitem concorrência no tratamento de requisições
+
+
+//PROVA3
+//\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\//
+memoria.pdf
+
+Gerenciamento de  memória
+
+- Bare memory
+- Monitor residente
+- Multiprogramação
+	- Partições fixas
+	- Partições variáveis
+	- Memória virtual
+		- Paginação
+		- Segmentação
+		- Segmentação paginada
+
+Monoprogramação sem Paginação ou Swapping
+	- Bare memory
+		não existe esquema de gerenciamento da memória. O usuário possui controle absoluto sobre todo o espaço de memória
+		não há sistema operacional
+
+	- Monitor residente
+		uma área para o usuário e outra para o sistema operacional
+		Hardware adicional:É necessário um dispositivo de hardware que impeça o processo de usuário de acessar a área do sistema operacional
+
+Multiprogramação
+	Como manter mais de um processo de usuário ativo em memória simultaneamente?
+	
+	- Partições fixas
+		O processo roda na mesma partição fixa até terminar
+		Fragmentação interna
+	
+	- Partições variáveis
+		O número, o tamanho e a localização dos processos em memória varia com o tempo
+		First fit, best fit, worst fit
+		Fragmentação externa 
+
+	- Memória virtual
+		Como executar um processo que não cabe inteiramente na memória?
+		devemos quebrar o processo em unidades menores e carregar estas unidades em memória à medida em que elas são necessárias
+		Com a memória virtual, o processo não referencia mais endereços físicos e sim endereços virtuais
+
+		- Paginação
+			divide a memória física e a memória virtual em unidades de tamanho fixo
+			O endereço virtual é dividido em unidades de tamanho fixo chamadas páginas
+			memória física é dividida em unidades de tamanho igual ao das páginas, chamadas page frames
+			Tabela foward: A tabela de páginas é um vetor de entradas, armazenada em memória.
+			Tabela invertida: classificada pela página física (frame) e não pela página virtual, ficando menor
+
+			Algoritmos realizáveis de substituição de páginas:
+				- NUR
+				- FIFO
+				- Segunda
+				- Relógio
+				- LRU
+
+		- Segmentação
+			divide a memória física e a memória virtual em unidades de tamanho variável
+			A segmentação facilita a implementação de bibliotecas compartilhadas. As bibliotecas compartilhadas não são
+				ligadas no código executável do programa. Seu código é colocado em um segmento, que pode ser compartilhado
+				por vários processos, em tempo de execução
+			paginar os segmentos
+			Exemplos típicos de segmentos são os segmentos de código, dados e pilha de um processo
+			somente as páginas realmente necessárias são mantidas em memória
+
+
+
+		- Segmentação paginada
+
+
+
+
+		
+
+
+
 
 
 							
