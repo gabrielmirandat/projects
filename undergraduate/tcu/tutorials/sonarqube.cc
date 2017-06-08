d3b66e65d55b022c3036bd3ce2f5ca626925a5a2
Gabriel Miranda <gabrielmirandatt@gmail.com>
2017-06-08 16:42:50 -0300

    Added sonar tutorial

---
+++ undergraduate/tcu/tutorials/sonarqube.cc
@@ -0,0 +1,26 @@
+https://docs.sonarqube.org/display/SCAN/Analyzing+with+SonarQube+Scanner
+
+SonarQube Scanner is recommended as the default launcher to analyze a project with SonarQube
+
+https://docs.sonarqube.org/display/SONAR/Get+Started+in+Two+Minutes
+
+2. Start the SonarQube server:
+'$ /etc/sonarqube/bin/[OS]/sonar.sh console'
+
+
+3. Configure and start Sonar Scanner
+// sonar-project.properties
+# must be unique in a given SonarQube instance
+sonar.projectKey=my:project
+# this is the name and version displayed in the SonarQube UI. Was mandatory prior to SonarQube 6.1.
+sonar.projectName=My project
+sonar.projectVersion=1.0
+ 
+# Path is relative to the sonar-project.properties file.
+# This property is optional if sonar.modules is set. 
+sonar.sources=.
+ 
+# Encoding of the source code. Default is default system encoding
+#sonar.sourceEncoding=UTF-8
+
+'$ sonar-scanner'
