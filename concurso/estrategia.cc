8216984cf1ed3adf761b798775efd2f97e367688
gabrielmirandat <gabrielmirandatt@gmail.com>
2017-06-15 17:30:50 -0300

    Nova estrategia de concurso

--- concurso/estrategia.cc
+++ concurso/estrategia.cc
@@ -1,16 +1,24 @@
-
-ESTRATEGIA 1 (começando a estudar pra concurso..)
+ESTRATEGIA
 	ETAPA 1
-		Pega uma prova
-		Fazer toda a prova
-		Checar resultado (anotar resultados para rastreio)
+		Para todo concurso relevante 
+		Olhar ultimas provas no QConcursos
+		Fazer um histograma dos assuntos que mais caem global e separada por concurso dos últimos anos
+		Lista de assuntos que mais caem em redações/itens que reprovam
 	FIM ETAPA1
 
 	ETAPA 2
-		Passar pela prova
-			anotar assunto de forma geral (QConcursos) e se preciso específica (com tag única para facilitar rastreio em fontes extensas)
-			estudar o assunto anotado
-			explicar porque a questao está certa ou errada
-		Fim prova
+		Pega uma prova
+		Fazer toda a prova
+		Checar resultado (ver quanto tiraria)
 	FIM ETAPA2
-FIM ESTRATEGIA
+
+	ETAPA 3
+		Passar pela prova
+			Estudar conteúdo da questão empesado na relevância do assunto de acordo com seu histograma e com a instituição
+			Explicar porque a questao está certa ou errada
+		Fim prova
+	FIM ETAPA3
+FIM ESTRATEGIA	
+
+
+GABARITOU TI
