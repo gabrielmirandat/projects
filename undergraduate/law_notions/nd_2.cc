d9149732dcbe1c847af24f9a0fbadad8296711b7
Gabriel Miranda <gabrielmirandatt@gmail.com>
2017-06-24 12:10:36 -0300

    Update nd_2.cc

--- undergraduate/law_notions/nd_2.cc
+++ undergraduate/law_notions/nd_2.cc
@@ -1,1 +1,56 @@
+TEORIA DA DECISÃO JUDICIAL E UM PANORAMA DO PODER JUDICIÁRIO
 
+Da Escola francesa da exegese
+  Positivismo exegético/explicativo - jurídico
+    - analise sintatica rigorosa suficiente para resolver qualquer problema de interpretação
+    - o juiz (intérprete) deve ater-se totalmente ao texto legal
+    
+Da Escola histórica
+  Direito é temporal (não é mais atemporal, imutável e eterno), vem do passado histórico
+    - Friedrich Carl von Savigny
+    - faz parte da cultura nacional, "espirito do povo"
+    - não se cria normas abstratas aplicáveis a todos indistintamente
+    - não existe um direito racional para todos os povos e culturas
+    
+Da Jurisprudência dos interesses
+  Influência sociológica
+    - falácia conceitual da jurisprudência dos conceitos
+    - o método lógico-formal é insuficiente para resolver conflitos na realidade social concreta
+    - ponderação entre interesses contrapostos para trazer paz almejada por todos
+    - técnica seria usada pela "jurisprudência dos valores"
+    - uma ação é moralmente correta, uma lei é justa, quando promove a felicidade do maior número de pessoas
+    
+Da Interpretação judicial como ato de vontade (Kelsen)
+  A jurisprudência dos interesses privilegia argumentos ideológicos e políticos na interpretação do direito
+    - reforça método analítico da jurisprudência dos conceitos
+    - ciência jurídica deve ser estudada sem interferências valorativas, como no contexto positivista
+    - o problema de interpretação é mais semântico que sintático
+    - a interpretação é um ato de vontade em que o juiz cria uma norma individual para o caso concreto
+    - isso é um subjetivismo do juiz dentro de uma moldura semântica da norma
+    - juiz deve obediência à moldura, mas dentro dela a interpretação é livre
+    
+Da “Textura aberta” de Herbert L. A. Hart
+  Hart quer mostrar a insuficiência da posição de Kelsen
+    - insuficiência do intérprete (juiz) como mero revelador do sentido do texto
+    - posições extremas, devendo encontrar um meio termo
+    - não só existem casos extremos, que sejam só fáceis ou difíceis
+    - legisladores não tem conhecimento de todas as possíveis circuntâncias fáticas que o futuro pode trazer
+    - proibir veículos no parque, carros faz sentido (fácil), mas e bicicleta? (difícil)
+    - textura aberta do direito = distinguir a zona clara da zona de penumbra
+    - na zona clara, resolver pela técnica mecanicista da subsunção
+    - na zona de penumbra, devido a indeterminação do texto legal são passíveis da discricionariedade do judicial
+    
+O Direito como integridade e a teoria da única resposta correta de Ronald Dworkin
+  Aproximação do direito e da moral crítica através de princípios
+    - integridade é uma virtude que exige compromisso com outros ideais
+    - integridade está relacionada à ideia de comunidade de princípios
+    - O direito como integridade seria uma via alternativa entre a corrente convencionalista e a pragmática
+    
+    
+
+
+
+
+
+    
+      
