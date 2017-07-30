c9991c9c754c9a0a0d09839e88ad31802dd39ea0
gabrielmirandat <gabrielmirandat@hotmail.com>
2017-07-30 18:50:12 -0300

    Added the particular project in arduino

---
+++ college/particular/public/arduino/cod4arduino_ino.ino
@@ -0,0 +1,22 @@
+
+
+int ldr=A0, potenciometro=A1,luz=0, pot=0, buzzer=10;
+
+void setup() {
+  pinMode(buzzer,OUTPUT);
+  pinMode(ldr,INPUT);
+  pinMode(potenciometro,INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  luz=analogRead(ldr);
+  pot=analogRead(potenciometro);
+  Serial.print(luz);
+  Serial.print(" -- ");
+  Serial.println(pot);
+  analogWrite(buzzer,0);
+  if(pot>500 && luz<200){
+    analogWrite(buzzer,100);
+  }
+}
---
+++ college/particular/public/arduino/cod5arduino.ino
@@ -0,0 +1,22 @@
+#include <Servo.h>
+
+int motor = 5;
+int luz = 0;
+int ldr=A0;
+int luzMap = 0;
+Servo ServoLegal;
+
+void setup() {
+  ServoLegal.attach(motor);
+  Serial.begin(9600); 
+  pinMode(motor,OUTPUT);
+  pinMode(ldr,INPUT);
+}
+
+void loop() {
+  luz=analogRead(ldr);
+  luzMap = map(luz, 0 , 1023, 0, 180);
+  Serial.println(luzMap);
+  ServoLegal.write(luzMap);
+  delay(1000);
+}
