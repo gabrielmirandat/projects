5d7cf77d83bc15ced1f9af70e989d671b993076c
gabrielmirandat <gabrielmirandat@hotmail.com>
2017-07-30 18:52:01 -0300

    Added the particular project in opengl

---
+++ college/particular/public/opengl/tutoOpenGL/prog1/prog1.cpp
@@ -0,0 +1,29 @@
+#include <GL/glut.h>
+
+// Função callback chamada para fazer o desenho
+void Desenha(void)
+{
+	//Limpa a janela de visualização com a cor de fundo especificada 
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	//Executa os comandos OpenGL 
+	glFlush();
+}
+
+// Inicializa parâmetros de rendering
+void Inicializa (void)
+{   
+    // Define a cor de fundo da janela de visualização como preta
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+}
+
+// Programa Principal 
+int main(int argc, char **argv)
+{
+        glutInit(&argc, argv);
+        glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+	glutCreateWindow("Primeiro Programa");
+	glutDisplayFunc(Desenha);
+	Inicializa();
+	glutMainLoop();
+}
