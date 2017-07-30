8db1af442220e4277d76830be1aab2e6df8481c1
gabrielmirandat <gabrielmirandat@hotmail.com>
2017-07-30 18:37:26 -0300

    Added ideas folder

---
+++ college/ideias/super-projetos.cc
@@ -0,0 +1,53 @@
+17:58 17/02/2017
+
+DRONE PARA NAVEGAR PELO MUNDO
+
+- Bateria solar
+- Comunicação via rede e satélite
+- o Robô me dá PM pela noite para eu posicioná-lo para descansar (ou outro)
+- Faz vídeos em 3D periódicos
+- Usa GPS para localização com odometria?
+
+
+SEGURANÇA - ROUBO DE CARRO E CELULAR
+
+- Ao comprar na concessionária, a pessoa registra o nome de todas as pessoas que podem usar o carro.
+  Fica guardada numa memória. A única forma de alterar dados na memória é pelo aplicativo na venda. Não 
+  deve ser possível formatar essa memória, nem retirá-la do slot. Se retirar do slot o carro fica inacessível. 
+  Usar ROM é possível? Checar. 
+
+- Ao ligar o carro com a chave, o motorista deve validar seus dados para poder andar com o carro com digital. Um ladrão pode ordenar 
+  a pessoa a validar e fingir que é o dono, mas aí se desligar o carro tem que validar de novo.
+
+- Para vender o carro, a primeira etapa é um sensor de dinheiro que deve ser passado num celular logado no app de segurança
+  para contabilizar o preço de venda. O carro vai ter um valor mínimo de venda por padrão. Tudo bem o vendedor querer vender mais barato, 
+  já que cada nota de mesmo valor contabiliza no sensor da mesma forma, basta passar a mesma nota várias vezes para 
+  contabilizar. A questão aqui é, como a maioria dos roubos é relâmpago e o ladrão não tem tempo de ficar passando nota, 
+  nesse meio tempo mil coisas podem acontecer, vou falar disso mais tarde. O objetivo é que o processo de venda demore um 
+  pouco mesmo, não se vende um carro o tempo todo, tenha paciência para na venda fazer esse processo. Depois de passar todas 
+  as notas para contabilizar o valor do carro, o dono master (venda só pode ser feita pelo dono master do carro) vai "resetar" 
+  as pessoas que podem usar o carro com o app e o comprador vai registrar as pessoas que podem usar o carro que ele está adquirindo.
+
+- Se uma pessoa for roubada. Vai ter que entrar no processo de compra para o ladrão pegar o carro. O processo deve demorar pelo 
+  menos meia hora. O ideal seria todas as pessoas terem o aplicativo de segurança. O app é referente ao dispositivo de segurança.
+  A pessoa deve estar logada no aplicativo. O carro só entra em processo de compra se a pessoa estiver logada no aplicativo. O aplicativo 
+  não tem muita restrição de quem pode logar, basta a pessoa estar logada, e deve ficar logada até o processo terminar. Dessa forma, sempre 
+  que alguém entrar em processo de venda/compra, pessoas ao redor vão receber uma mensagem com as coordenadas de quem está vendendo. Não é 
+  o tempo todo que vamos ver algém vendendo numa redondeza, e as pessoas não vendem carros no meio da rua, nem celular. Assim pessoas próximas 
+  podem ir olhar se está tudo bem, se aparecer gente não vai dar tempo de o ladrão roubar, vão chamar a polícia ou qualquer coisa assim. 
+
+- Seria interessante também ter uma forma fácil de avisar a polícia, como apertar um botão no aplicativo de segurança. Uma pessoa que pode 
+  ter visto pode apertar o botão do aplicativo de segurança dela. Avisando um posto policial próximo.
+
+- Não é necessário ter um carro para ter uma conta no aplicativo. A única diferença é que pessoas logadas que tenham carro vai mostrar o carro 
+  dela no app, aí ela clica nele pra poder vender. O app só tem duas telas. Tela de venda, e tela de google maps mostrando pessoas próximas e 
+  localizações que tenham carro. O processo de venda fica registrado. O app mostra a pessoa que está dirigindo e a pessoa que deveria estar 
+  dirigindo. Se algém olhar pra pessoa que está dirigindo e não parecer a pessoa do aplicativo com certeza o carro é roubado.
+
+- Se o ladrão roubar o celular e o carro, vai estar logado na conta da pessoa. A conta pode se deslogar a cada dois dias. E se o ladrão exigir
+  a senha no roubo, o dono pode trocar a senha depois, usando chave de segurança pre-definida.
+
+
+  PROBLEMATICO
+
+  Usa algumas coisas, registro com digital e reconhecimento de face, braço sei la.
