67049942beb53a261635b437a9d0435d6ffc5c77
gabrielmirandat <gabrielmirandat@hotmail.com>
2017-07-30 18:46:52 -0300

    Added files for the internet of things course

---
+++ college/particular/internet_courses/internet_things.cpp
@@ -0,0 +1,65 @@
+'IoT' (Internet of Things), which will be used to connect all
+objects through the Internet for remote sensing and control.
+
+'IoT' & 'M2M Ecosystem'.
+Application Layer, 
+Management Service Layer, 
+Gateway & Network Layer, 
+and Sensor Layer are explained.
+
+IoT Technologies and 
+'R&D' (Research & Development) 
+
+IoT supportive wireless networking technologies.
+
+Smartphones, OS, Cloud Computing, Big Data, CDN, Wi-Fi, Bluetooth, Mobile Communication, 
+LTE, LTE-Advanced, IoT, AR, IPv4, IPv6, TCP, UDP, and Internet operations.
+
+'IoT' (including IoT wireless networks and services) and 
+'AR' (including AR processes and feature extraction techniques) 
+and how they can work together to provide services to our smartphones, laptops, PCs, and TVs.
+
+Module 1 : IoT (Internet of Things)
+
+Key Concepts:
+IoT Architecture
+Application Layer
+Management Service Layer
+Gateway & Network Layer
+Sensor Layer
+IoT wireless networking
+
+Module 2 : AR (Augmented Reality)
+
+Key Concepts:
+
+AR Process
+Image Acquisition
+Feature Extraction
+Feature Matching
+Geometric Verification
+Associated Information Retrieval
+
+IoT
+-> Tudo sera gerenciado remotamente
+-> necessidade de cloud computing - para armazenar dados brutos dos sensores.
+
+layers em ordem:
+4.Application - energy, environment, healthcare, transportation, supply chain, retail, people tracking, endless application
+3.Management Service - device modelling configuring and management, dataflow management, security control
+2.Gateway and Network - wide area network, mobile communication network, wifi ethernet, gateway control
+1.Sensors Connectivity and Network - sensor network, sensors, actuators, tags (RFID and barcodes)
+
+RFID - radio frequency identification
+
+>>IoT Networks
+
+
+
+
+
+
+
+
+
+
