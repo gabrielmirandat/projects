27421a290b3c33f347ce40e39ef587d7961be62a
gabrielmirandat <gabrielmirandat@hotmail.com>
2017-07-30 18:38:01 -0300

    Added tutorials folder

---
+++ college/tutorials/software_tutorials/ros_intermediate.cc
@@ -0,0 +1,10 @@
+'Pacote na mão' - "http://wiki.ros.org/catkin/CMakeLists.txt"
+$ rospack find foobar 					# lista dependências de um pacote (package.xml)
+
+'Gerenciar dependências do sistema' - chaca as dependências no package.xml
+$ rosdep install [package] 				# instala dependências de package
+$ rosdep install turtlesim 				# download e instala dependências de turtlesim
+
+'Using roslaunch to start Gazebo, world files and URDF models' - http://gazebosim.org/tutorials?tut=ros_roslaunch
+http://wiki.ros.org/roslaunch/XML
+
