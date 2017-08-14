c328e567839717623e2525e51a5d27fbc3a55755
Gabriel Miranda <gabrielmirandatt@gmail.com>
2017-08-14 11:42:46 -0300

    Adicionado edital e estudo

---
+++ concurso/EDITAIS/conteudo_edital.cc
@@ -0,0 +1,103 @@
+CONHECIMENTOS BÁSICOS
+PARA TODOS OS CARGOS DE ANALISTA JUDICIÁRIO,
+EXCETO PARA O CARGO DE ANALISTA JUDICIÁRIO – ÁREA APOIO ESPECIALIZADO – ESPECIALIDADE TAQUIGRAFIA
+
+# Língua Portuguesa 
+Interpretação de texto. 
+Argumentação. Pressupostos e subentendidos. 
+Níveis de linguagem. 
+Articulação do texto: coesão e coerência. 
+Termos da oração. 
+Processos de coordenação e subordinação. 
+Discurso direto e indireto. 
+Tempos, modos e vozes verbais. 
+Classes de palavras. 
+Flexão nominal e verbal. 
+Concordância nominal e verbal. 
+Regência nominal e verbal. 
+Ocorrência da Crase. 
+Ortografia e acentuação.
+Pontuação. 
+Equivalência e transformação de estruturas. 
+Redação.
+
+# Noções sobre Direitos das Pessoas com Deficiência
+(Resolução CNJ no 230/2016 - art. 19): 
+	Inclusão, direitos e garantias legais e constitucionais das pessoas com deficiência 
+	(Lei no 13.146/2015; Lei no 11.126/2005 e Constituição Federal). 
+Normas gerais e critérios básicos para a promoção da acessibilidade das pessoas portadoras
+	de deficiência ou com mobilidade reduzida (Lei no 10.098/2000 e Decreto 5.296/2004). 
+Prioridade de atendimento às pessoas portadoras de deficiência (Lei no 10.048/2000 e Decreto 5.296/2004). 
+Direitos no sistema de transporte coletivo (Lei no 8.899/1994) e Decreto 3.691/2000). 
+Símbolo de identificação de pessoas portadoras de deficiência auditiva (Lei no 8.160/1991). 
+Normas de apoio às pessoas portadoras de deficiência e sua integração social (Lei no 7.853/1989 e Decreto 3.298/1999).
+
+# Regimento Interno do TST 
+Aprovado pela Resolução Administrativa no 1.295/2008 com alterações dos 
+	Atos Regimentais nos 1/2011, 2/2011 e 3/2012 e Emendas Regimentais nos 1/2011, 2/2011 e 3/2012.
+
+CONHECIMENTOS ESPECÍFICOS
+C03 - ANALISTA JUDICIÁRIO – ÁREA APOIO ESPECIALIZADO – ESPECIALIDADE ANÁLISE DE SISTEMAS
+
+# Bancos de Dados
+Características de um SGBD.
+Modelo relacional.
+Modelagem de bancos de dados.
+Modelo Entidade-Relacionamento.
+Normalização de Banco de Dados.
+SQL.
+Oracle PL/SQL, SQL Server.
+Princípios sobre administração de bancos de dados.
+
+# Engenharia e Desenvolvimento de Software
+Princípios de engenharia de software.
+Ciclo de vida de sistemas.
+Processos de software.
+Métodos ágeis (eXtreme Programming – XP, Scrum, Kanban).
+Modelos de desenvolvimento de sistemas.
+Engenharia de requisitos.
+Testes de software.
+Princípios sobre qualidade de software: modelo MPS.BR e CMMI-DEV versão 1.3.
+Análise e projeto estruturados de sistemas.
+Modelagem de processos (BPMN).
+Modelagem funcional.
+Modelagem de dados.
+Modelagem orientada a objetos.
+Conceitos sobre orientação a objetos (classes, atributos, objetos, métodos, herança, interfaces e outras hierarquias).
+Análise e projeto orientados a objetos (UML).
+Estruturas de dados e de controle de fluxo.
+Funções e procedimentos.
+Controle de versões com Git e SVN.
+Conceitos de linguagens estruturadas.
+Conceitos de linguagens orientadas a objetos.
+Conceitos de Web Services SOAP e REST.
+Aspectos gerais das linguagens PHP, HTML5, XML, Java SE, Java EE, JUnit.
+Padrões de Projeto (design patterns).
+Conceitos sobre desenvolvimento Web e cliente/servidor.
+Web Standards, W3C; CSS3, Javascript (ECMAScript 6), bootstrap, jquery.
+
+# Sistemas de apoio à decisão
+Arquitetura OLAP, ETL, Data Mining, Data Warehouse.
+ERP.
+Business Intelligence: Conceitos Princípios e Processos.
+
+# Governança e Gestão de TI
+Planejamento estratégico de TI – PETI.
+Alinhamento estratégico entre áreas de TI e negócios.
+Políticas e procedimentos.
+Estrutura organizacional e responsabilidades da TI.
+Responsabilidade e papéis da TI.
+Segregação de funções de TI.
+Controles de segregação de funções.
+Processos de definição, implantação e gestão de políticas organizacionais.
+Práticas de gestão da TI.
+Gestão de pessoal.
+Terceirização da TI.
+Gestão de mudanças organizacionais.
+Gestão de riscos.
+Programa de gestão de riscos.
+Processo de gestão de riscos.
+Métodos de análise de riscos.
+Governança e Gestão de TI com COBIT 5 - Estrutura, objetivos e conceitos gerais.
+Gestão de serviços de TI com ITIL v3 Edição 2011 - Estrutura, objetivos e conceitos gerais.
+Gestão de projetos com PMBOK 5a Edição.
