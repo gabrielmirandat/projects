3aa768a77a2d3cdadcfc27d445afba859934b3fc
gabrielmirandat <gabrielmirandatt@gmail.com>
2017-08-14 01:13:21 -0300

    Added files of tcc

---
+++ tcc/monograph/tips_helper_writing.cc
@@ -0,0 +1,12 @@
+# ESPAÇO DE DICAS E RECURSOS
+- Pelo menos uma citação por página
+
+## Recursos de uma trabalho de tcc
+- Introdução
+- 
+
+## Recursos da classe UnB/Cic
+- Imagens
+- Citação
+- Apêndice
+- Documentação
