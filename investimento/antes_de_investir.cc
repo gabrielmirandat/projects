aec2a49c71d517b0e2a5d94d179428001175f46b
gabrielmirandat <gabrielmirandatt@gmail.com>
2017-08-14 01:06:14 -0300

    Adicionados arquivos em investimentos

---
+++ investimento/antes_de_investir.cc
@@ -0,0 +1,24 @@
+---------PERCA O MEDO DE INVESTIR!
+Taxa SELIC
+	- Mãe de todas as taxas
+	- Comando todos os juros - é um indexador
+	- Quanto mais alta a taxa SELIC, maior os juros são
+
+IPCA (Indice de Preços as Consumidor)
+	- Inflação
+	- Se tiver alto, precisa guardar mais dinheiro para comprar o mesmo produto
+
+CDB
+	- Títulos para quando um banco precisa de dinheiro
+
+Título de Capitalização (Não é investimento)
+	- É a telesena
+	
+Poupança
+	- Rende menos que a inflação
+
+Tesouro direto
+	- Empresta dinheiro para o governo
+
+FGC (Fundo Garantidor de Crédito)
+	- Garante seu dinheiro de volta se o banco quebrar
