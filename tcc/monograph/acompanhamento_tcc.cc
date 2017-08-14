36ef01163d4fb561ee013c4e281fd471f7acd47a
gabrielmirandat <gabrielmirandatt@gmail.com>
2017-08-14 18:21:49 -0300

    Added acompanhamento tcc

---
+++ tcc/monograph/acompanhamento_tcc.cc
@@ -0,0 +1,23 @@
+SEMANA 0
+- Baixar template da monografia no overleaf (mostrar overleaf capítulo e citação)
+- Ler template
+- Separação inicial das partes da monografia
+- Configurar mendeley para citações no template (mostrar mendeley artigos)
+- Baixados artigos iniciais
+- Comecei a ler artigo de algoritmos de Slam no Ros, já escrevendo na monografia
+
+Objetivo: adiantar ao maximo a escrita e ir fazendo junto
+
+# Perguntas
+1. Devo usar um mesmo artigo em partes diferentes da monografia? 
+Usar a introdução dele por exemplo numa sessão da monografia, ou achar artigos específicos que já introduzem o assunto?
+ex. Usar a introdução do artigo SLAM com ROS na introdução da minha monografia como citação
+
+2. Posso citar o livro do Thrun ou só artigos?
+
+SEMANA 1
+- Terminar artigos ROS SLAM
+- Testar os algoritmos mostrados em simulação 
+- Testar se der no robô
+- Tentar encontrar o melhor para o nosso caso (no prédio do cic com landmarks no chão)
+- Fazer análise qualitativa entre eles
--- tcc/monograph/questions.cc
+++ tcc/monograph/questions.cc
@@ -1,3 +1,1 @@
-1- Devo usar um mesmo artigo em partes diferentes da monografia? Usar a introdução dele por exemplo numa sessão da monografia, ou achar 
-artigos específicos que já introduzem o assunto?
-ex. Usar a introdução do artigo SLAM com ROS na introdução da minha monografia como citação
+1- 
