0e2861d3906b99c6b620b643cfc69e950bfa9804
gabrielmirandat <gabrielmirandatt@gmail.com>
2017-08-14 23:17:30 -0300

    Adicionado conteudo do intensivo tst

---
+++ concurso/EDITAIS/EM ESTUDO/TST - TRIBUNAL SUPERIOR DO TRABALHO/2017_FCC_ANALISTA_DE_SISTEMAS/palestras_intensivo_estratégia/14_08_regimento_interno_do_tst.cc
@@ -0,0 +1,377 @@
+# Regimento Interno do TST 
+Aprovado pela Resolução Administrativa no 1.295/2008 com alterações dos 
+	Atos Regimentais nos 1/2011, 2/2011 e 3/2012 e Emendas Regimentais nos 1/2011, 2/2011 e 3/2012.
+
+Fabrício Rego - STDF
+TST - poder judiciário
+
+Organização
+Funcionamento
+Orgaoes
+Sessoes
+Processos
+Administração
+Alteração do Regimento
+
+Cobrado na literalidade e integralidade
+Ler a lei na fonte
+
+
+Cai 40% - artigos Do 1 ao 50
+Porem +200 artigos
+Competências dos orgaoes sempre caem
+	Presidente
+	Vice-presidente
+
+# ESTRUTURA DO TRIBUNAL
+
+Alterações constitucionais
+	A111-A. 
+		TST: 27 ministros, brasileiro, +35-65, notavel saber juridico, reputação ilibada (de acordo com constituição)
+			 nomeados pelo presidente da republica nomeados apos aprovação do senado federal
+		par3: TST pode processar e julgar a reclamação para preservar a si
+
+	Regimento interno (de acordo com regimento)
+		A3. 27 ministros, brasileiro, +35-65 pelo STF!
+
+	Composição do tribunal (27 membros)
+		1/5 de advogados +10anos e membros do mp do trabalho (mpt) +10anos (25% vagas)
+		demais (magistrados) juizes dos tribunais regionais do trabalho indicados pelo TST
+			desembargador do tribunal ou juiz do trabalho
+
+	Do quinto do tribunal
+		a vaga é pra cada uma das classes, vaga MP, vaga ...
+		se sobrou MP, MP manda lista com 6 nomes
+		TST faz redução dessa lista
+
+		para reduzir
+			TST por voto secreto da maioria absoluta dos nomes
+			se mais de uma vaga, lista triplice para cada vaga  (TRIPLICE PARA CADA VAGA PARA CADA UMA DAS LISTAS SEXTUPLAS)
+			se receber lista unica sera triplice num vagas mais dois
+
+		Membro OAB/MP
+			será formada uma triplice para cada uma das SEXTUPLAS
+
+		Se receber apenas uma lista unica para mais de uma vaga, uma só lista com o num de candidatos mais dois
+
+
+
+	Das vagas dos magistrados
+		lista triplice com 3 nomes feito pelo proprio TST enviados ao presidente da republica
+		se tem 3 vagas de ministro, faz lista triplice + 2 (NUM VAGAS + 2)
+			se 1 vaga, lista triplice
+			se 2 vagas, lista com 4 nomes
+			se 3 vagas, lista com 5 nomes
+
+		o TST se reunirá no plenário para decidir em votos secretos
+			a votação é pelo cargo vago (escolhe os que vão pra primeira vaga, depois escolhe os que vão pra segunda vaga), sucessivo
+			pra cada um dos candidatos, escolhe maioria absoluta, 50% + 1 dos presentes na corte da votação (NÃO É DOS MEMBROS DO TST)
+			senão alcançar a maioria absoluta, concorre os dois juizes mais votados em nova eleição
+			se empate, novas eleições, se persistir, pega o tempo de investidura no tribunal ou magistratura
+			votação para desempate
+
+Antiguidade (requisito importante pra tudo)
+	não significa o mais velho, e sim aquele que é mais antigo
+	tempo de tribunal, quando o ministro tomou a posse começa a contar
+
+	ordem
+		Posse
+		Nomeação
+		Tempo de magistratura do trabalho
+		Tempo de serviço publico federal
+		Pela idade
+
+# DIREÇÂO DO TRIBUNAL
+Orgaoes e cargos
+
+Orgaoes
+	Presidencia
+	Vice-presidente
+	Corregedoria-geral da justiça do trabalho (cargos eleitos)
+
+	Eleitos de acordo com a Antiguidade! pra quem ainda não foi, um presidente pode deixar de ser (artigo 102)
+	Irrepetibilidade, mesmo ministro não deve ocupar o mesmo cargo (pq tem muito ministro)
+	Escrutíneo secreto (votação secreta) - por questão formal
+	Vota da maioria absoluta (50%+1) (é antiguidade mas necessaria)
+	Seção extraordinária do TST
+	Realizada nos 60 dias antes do termino do mandato anterior
+	Toma posse em data especifica seção solene em data imposta pelo tribunal
+	Se vagar antes o cargo de presidente, deve haver eleições para todos os cargos
+
+	Se impossibilidade de assumir
+		Presidente - nova eleição para todos
+		Vice-presidente - nova eleição de vice e para Corregedoria-geral
+		Corregedoria-geral - nova eleição só para Corregedoria-geral (corregedor)
+
+	Os mais antigos que podem ocupar os cargos, eleição por questão formal
+
+
+# COMISSOES DO TRIBUNAL
+	São braços, não orgaoes, formadas por ministros mas não julgam processos
+	Auxiliam e suportam as atribuiçoes do tribunal
+	Colaborar no desempenhos
+	Composta por ministros eleitos para as comissões permanentes 
+		(tribunais com mais de 25 membros podem compor orgao especial para descentralizar atividades administrativas do tribunal pleno)
+
+	São 3
+		De regimento interno 
+			- zela os processos dentro do tribunal
+		De jurisprudencia e precedentes normativos 
+			- zela a jurisprudencia, julgados, reiterados, compilação para facilitar uso, pode sugerir edição de sumula, OJs, precedentes, estudo e sugestão
+			- quem tem a palavra é o tribunal pleno
+		De documentação
+			- cuida da biblioteca, arquivamento, processos, administração disso tudo
+
+		Cargos de direção (Presidente, vice, ...) + diretor e vice da ENAMAT não integram comissões permanentes! 
+		Em temporarias podem compor
+		ENAMAT funciona junto com o tribunal
+
+		Orgao especial elege os ministros que farão parte, sendo o mais antigo o presidente seguindo a regra de antiguidade
+		A corte dos membros mais antigos forma o orgao especial
+		Nenhuma das comissões esta dentro do orgão, nenhuma julga processos
+
+
+# ORGANIZAÇÂO DO TRIBUNAL
+	Orgaoes do TST(JUDICANTES - SÃO FIMs - julgam)
+		1.Tribunal pleno
+			plenitude dos membros - 27 ministros
+		2.Orgao especial
+			tribunal pleno reduzido, funcionamento com menos ministros - tem status de julgar de forma descentralizada
+		3.Seção especializada em dissidious coletivos (é unica)
+		4.Seção especializada em dissidious individuais (2 subsessões)		
+		5.Turmas
+
+	Maior parte dos processos  é 3,4 ao 5 (órgãos fracionários, colegiados, mais de um ministro)
+
+		Orgoes que funcionam junto ao TST
+			1. ENAMAT
+			2. CSJT (fiscalização e controle do TST) como um CNJ
+
+	Ministro pode escolher onde atuar entre 3,4 e 5? Pode se respeitar ordem de antiguidade
+		Também pode se haver permuta de turmas - troca
+			Os presidentes de turma devem antes renunciar a presidencia do colegiado (vota aprovando ou reprovando)
+			Deve renunciar a presidencia da turma
+		Cada ministro compoe apenas uma seção especializada
+
+	1. Tribunal pleno
+		- composição total dos ministros
+		- quorum
+			- de abertura de trabalhos (minino de presentes para abertura de um trabalho)
+			- minimo para aprovação de uma matéria
+		- para abrir um trabalho deve ter no minimo 14 ministros presentes (maioria simples ou relativa - ou seja, dos presentes)
+		- já para as deliberações, pela maioria absoluta dos membros 27/2 + 1 para aprovar/deliberar escolha de nomes 
+			- emenda regimental, eleição dos ministros para direção do TST, ...
+
+	2. Órgao especial (mais de 25 membros podem ter orgao especial)
+		- descentralização do tribunal pleno
+		- composto por membros da direção, sete ministros mais antigos incluindo os membros, sete ministros eleitos pelo tribunal pleno
+		- podem compor outras sessões do tribunal
+		- o quorum de funcionamento exige presença no minimo de oito ministros, absoluta se tratar de aposentadoria de magistrado
+
+	3.Seção especializada em dissidious coletivos (é unica)
+		- presidente, vice, Corregedoria-geral + 6 ministros
+		- quorum de abertura é de 5 ministros
+
+	4. SDI
+		- plena de 21 ministros ou subsessões fracionarias para julgamente de processos de sua competencia
+		- presidente, vice, Corregedoria-geral + 18 ministros
+		- quorum de funcionamento de 11 ministros (para o inicio dela)
+		- deliberações por maioria absoluta do integrantes da seção
+
+		SS1
+			- três básicos + 11 ministros (14 ministros)
+			- entre os 11 presidentes de turma, sendo exigidos 8 para funcionamneto
+		SS2
+			- três básicos + 7 ministros (10 ministros)
+			- sendo exigidos 6 para funcionamneto
+
+	5.Turmas
+		- cada uma tem 3 ministros
+		- ENAMAT
+			- junto ao TST
+			- autonomia administrativa
+			- regulamenta cursos para magistratura do trabalho (juiz)
+			- diretor, vice, eleitos pelo pleno, voto
+		- CSJT
+			- autonimia
+			- administrativa, orçamentaria, fiscalização e controle
+
+
+	Presidencia das Sessoes
+		- O Presidente preside todas das quais ele participa
+		- substituido pelo vice, corregedor, ministro mais antigo PRESENTE NA SEÇÂO
+
+	Presidencia das Turmas
+		- Por antiguidade, presidente não tá aqui
+		- Periodo de 2 anos, vedada a recondução enquanto não houver o rodizio
+
+	Presidencia do Ministerio Publico do trabalho (MPT)
+		- atua junto ao TST
+		- todo orgao tem um MP que atua junto
+		- é a parte acusatória
+		- relatores do trabalho
+		- o MPT precisa dar parecer obrigatoriamente em casos estrangeiros
+		- facultativamente por iniciativa do proprio relator com relevancia
+		- o proprio MP pode pedir também
+		- ou por mandados de segurança, ações civis publicas, dissidios coletivos originários, indios e comunidades em que o MP não foi autor
+
+		NÃO são remetidos à PGT-MP
+			- processos em que foram autora, não faz sentido dar parecer se foi quem iniciou
+			- remessa facultativa que exijam urgencia ou ja resolvidos na jurisprudencia do tribunal
+
+	# Processo no TST
+	- duas formas: natureza recursal (recurso) ou natureza originária (que começam no TST)
+	- processo é registrado e classificado (dissídio coletivo, de revista, nomeados!)
+	- classificado, numerado, distribuido aos ministros (por um sistema de TI e de forma aleatória proporcional)
+	- depois distribuido, o ministro faz a relatoria, levado para julgamento, competencia da turma, ou orgão especial, ou outro colegiado
+	- passa pelos ministros que vão votando, sendo contra ou a favor do relator
+	- alguns processos passam por um revisor, "um segundo relator" outro ministro que da parecer em determinados processos, todos tem relator, mas não revisor
+	- esse revisor ministro reavalia e emite o seu voto
+	- revisor estabelecido para os processos mais complexos
+
+	- Todos os ministros concorrem para a distribuição, menos os membros da direção (presidente, vice, corregedor)
+	- salvo exceção se for direcionado à direção
+	- Não são distribuidos para ministros que vão se aposentar em 60 dias
+	- durante ferias dos ministros so distribuem 4 tipos de processos
+		- dissíduo coletivo (de greves)
+		- mandado de segurança
+		- ações cautelres (generico para ações que protegem em relação a danos)
+		- habeas corpus
+		
+	- Processos ficam vinculados a ministros mesmo em caso de afastamento
+		- exceção
+	 	- dissíduo coletivo (de greves)
+	 	- mandado de segurança originário
+	 	- ações cautelres (generico para ações que protegem em relação a danos)
+	 	- habeas corpus
+	 	- a não ser que a propria parte peça para trocar por recompansação.
+	 	- na recompansação, quando o ministro voltar vai receber mais processos pelo que foi trocado para outro ministro
+
+	- Regras de distribuição
+		- nas Turmas, se afastar por 30 dias ou morte, ..., um desembargador é convocado para substitui-lo. 
+		- ao retornar do afastamento ou meu substituto entrar, vamos pegar todos os demais processos
+		- nas SE, é mesma coisa da Turma, troca de cadeira, o novo ministro pega todos os processos
+		- para Orgao Especial, no afastamento é atribuido ao ministro que o substituir, mesma coisa, art. 93
+		- se ministro trocar de órgao por permuta, troca as cadeiras e ambos assumem os processos do outro
+			- se já colocou o visto como relator, continua fazendo parte para apresentar aos colegas
+		- nas Turmas, pelo já conhecimento de um processo, os posteriores no mesmo assunto vão pra ela
+
+	- Pauta
+		- se processo apto a ser julgado, é colocado em mesa para ser pautado e depois julgado
+		- Independem de pauta (não vai pra agenda, vai direto para julgamento)
+			- demando cuidado especial
+			- recurso de revista, agravo de instrumento
+			- embargo/desembargo de declaração
+			- pedido de homologação de acordo originario ou grau recursal
+			- incidentes de suspensão
+		- para incluir na pauta, ordem de remessa à Secretaria
+		- preferenciais (já podem ser pautados sem esperar na agenda)
+			- futuro afastamento do relator
+			- relator ou propria parte pede preferencia
+			-  natureza do processo exige, dissídios coletivos, etc..
+			- transferencia de relator para outro colegiado
+			- submetidos ao rito sumarissimo, pesosas de mais de 60 anos de ideia
+
+
+# SESSÔES DO TRIBUNAL
+
+	Distribuições dos lugares
+Ministerios Publicos do Trabalho		DIR-Presidente-ESQ
+										   Vice/\Ministro mais antigo
+  									  Corregedor Segundo Ministro mais antigo
+
+ 	- Não se aplica as turmas
+ 	- Art 116 - No centro o presidente, direita o vice, esqueda ministro mais antigo, direita corregedor, esquerda segundo ministro mais antigo
+ 	- Ordem cruzada
+ 	- Ao lado direito do presidente direto, o Ministerio publico do trabalho
+ 	- Nos órgãos judicantes, os trabalhos seguem a seguinte ordem
+ 		- Verifica o num de ministros presentes (QUORUN)
+ 		- exame de propostas (preferencia, pedido de sustentação oral, .., sumula)
+ 		- julgamento de processo 
+ 	- Ordem do julgamento dos processos
+ 		- habeas corpus
+ 		- pedido de preferencia por advogado antes 30 minutos antes do julgamento, sendo que o advogado deve permanecer na seção de julgamento
+ 		- mandados de segurança e medidas cautelares
+ 		- remanescente de sessões anteriores
+ 		- demais processos
+ 	- Apregoa do processo (chamar a julgamento)
+ 		- após pautado, já tenho a lista dos processos, ao ler, vou apregoar, processo num tal, partes são tais, ministro tal, chamar a julgamento
+ 		- relator manifesta-se previamente, o que aconoteceu no processo, incidentes, documentos, antecipa um pouco do voto, mas não vota
+ 		- advogados fazem sustentação oral
+ 		- depois disso, relator vota oficialmente
+ 		- demais ministros votam
+ 		- presidente vota por último
+ 		- se não houver divergencia, presidente lança resultado
+ 		- ordem de votação é pela antiguidade
+ 	- Da Vista, ministro não principal pode pedir vista do processo para entender mais sobre ele
+		- em mesa, na mesma sessão, se declara entendedor
+ 		- regimental, ele pega e estuda por mais tempo até a próxima sessão, pedido mediamente justificado, 10 dias
+ 			- 10 dias para mostrar voto vista, pode ser prorrogado, não impede o mais moderno antes de mim de votar
+ 		- pedidos simultaneos, vistas sucessivas
+ 		- ministros afastados tem seus pedidos de vista desconsiderados
+ 		- se vista regimental, não precisa mais ser pautado, nem re-votar
+ 		- se trocar ministro, tem a relatoria novamente
+ 		- no afastamento definitivo sem concluir julgamente, continua de onde parou
+ 			- votos ficam válidos mesmo quando o ministro sair do cargo, o voto foi oficial
+ 		- se ministro não participou desde o inicio, não vota, a não ser que seja necessário, ele se considere conhecedor da causa
+ 			- se houver obrigatoriedade, renova sustentação oral, le relatorio novamente para poder votar
+ 	- Deliberação
+ 		- quorum é a maioria dos presentes
+ 		- se empate, voto do presidente é o voto de minerva no colegiado
+ 		- voto vencido do ministro, se perde e pos para julgamento
+ 			- rel pra acordam, ministro que venceu o vencido e processo voltou a julgamento
+ 	- Processos sem sustentação oral
+ 		- embargos
+ 		- conflito de competencia
+ 		- agravo de instrumento
+ 		- o advogado não pode fazer sustentação oral
+ 	- Participação de advogado
+ 		- advogado pode desistir na sustentação antecipada do ministro, mas ainda pode fazer no decorrer do processo
+ 		- primeiro fala o advogado do recorrente, se ambos o forem, o do reclamante
+ 		- litisconsortes, multiplos autores ou reus, prorroga ate 20 minutos
+ 		- MP pode falar oral
+ 		- Presidente do orgao pode cassar oral dos outros se inadequada
+ 	- Das sessões solenes
+ 		- os 3 máximos, para dar posse aos 3, dar posse do Ministro, celebrar acontecimento de alta relevancia
+ 	- Das publicações
+ 		- dissiduo coletivo publica acordam(termino de julgamente, forma acordam, resumo da desição do colegiado) na integra
+ 		- das demais, publica só resumo/ementa e parte dispositiva, a decisão por si só
+ 			- requisitos dos acordam
+ 				- ementa
+ 				- relatorio
+ 				- fundamentos em que se baseia a decisão
+ 				- o dispositivo (o que realmente importa, decisão propriamente dita)
+
+
+# CARGOS ADMINISTRATIVOS MAIS IMPORTANTES
+ 	- Cargos ocupados por servidores
+ 	- DG (diretor-geral)
+ 		- dirigir serviços adms do tribunal, braço direito do presidente
+ 		- ocupado por qualquer pessoa com requisitos basicos
+ 	- Secretario geral judiciario
+ 		- serviços judiciario (documentação, registro)
+ 	- Secretario Geral da presidencia
+ 		- assessoramento juridico (dirige gabinete da presidencia)
+ 	- Não pode nomear conjuge, companheiro de terceiro grau para cargo de comissão
+ 	- Servidor pode ocupar esses cargos, mas não ao lado do ministro parente
+
+ 	- Gabinete dos ministros
+ 		- chefe de gabinete (direito)
+ 		- assessores (direito)
+ 		- auxiliares de confiança (funçao comissionada, aprovada por orgao especial)
+
+ 	Emendas ao regimento
+ 		- só o tribunal pleno
+ 		- emenda regimental 
+ 			- modifica de forma mais consistente
+ 		- regimental
+ 			- só suprime ou acrescenta
+
+ 	Tribunal JULGA
+ 			 ADMINISTRA funcionarios e LEGISLA por meio de resoluções
+
+ 		- resoluções
+ 			- administrativas
+ 				- pessoal, atribuição de servidores, atos individuais e concretos, nomeadção para cargo
+ 			- Resolução
+ 				- deliberações, súmulas, atos normativas gerais, lato sensu
