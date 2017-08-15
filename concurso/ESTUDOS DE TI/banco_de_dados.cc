23a47fb44c2344494718d21f8264c806bfc8ccb1
gabrielmirandat <gabrielmirandatt@gmail.com>
2017-08-15 11:09:49 -0300

    Adicionado estudos em bancos de dados para TST

---
+++ concurso/ESTUDOS DE TI/banco_de_dados.cc
@@ -0,0 +1,37 @@
+# Questões de Concursos - filtro salvo como  'FCC - Banco de Dados'
+Justaposição é um processo de formação de coisas por composição, ou seja, ocorre a fusão das coisas
+
+- Um Administrador de bancos de dados e tuning
+	- tem por função organizar o sistema de BD para melhor desempenho
+	- tem por função ajuste fino quando mudança de requisitos
+
+- Tuning 
+	- é propor e aplicar mudanças visando otimizar o desempenho na recuperação ou atualização de dados, otimização
+	- Objetiva minimizar tempo de resposta e recuperação dos dados das aplicações
+	- Um dos fatores usados no módulo de tuning em um SGBD é avaliar o número de acessos a disco para realizar uma consulta
+
+- Uma instancia do banco de dados
+	- é a coleção das informações armazenadas nele em determinado momento
+
+- O SGBD Microsoft SQL Server 2008
+	- possui uma função sys.dm_index_physical_stats que retorna o nível de fragmentação de índices do banco de dados (status físico do indice do modelo de dados)
+
+- Bancos de dados normalizados
+	- tem por característica várias tabelas estreitas com poucas colunas
+
+Um SGBD (Sistema Gerenciador de Bancos de Dados)
+	- (Navathe) é uma coleção de programas que permite aos usuários criar e manter um banco de dados
+	- (Slberschatz) é constituído por um conjunto de dados associados a um conjunto de programas para acesso a esses dados
+	- (Carlos Heuser) é um software que incorpora as funções de definição, recuperação e alteração de dados em um banco de dados
+
+Um SBD (Sistema de Bancos de Dados)
+	- é a junção de BD + SGBD
+	- tem propriedades ACID (Atomicidade, Consistência, Isolamento, Durabilidade)
+	- tem por característica que a recuperação de falhas nas transações é mais complexa que o controle de concorrência
+
+- Um SGBD relacional
+	- deve possibilitar a identificação única de uma linha de uma tabela
+
+- Triggers
+	- são objetos armazenados que são automaticamente executados em resposta à execução de algum evento
+	- é um código procedural que é automaticamente executado em resposta a certos eventos numa tabela, view ou banco de dados
