82bfa22b499b67102c5dd675213f83d02809c6dd
gabrielmirandat <gabrielmirandatt@gmail.com>
2017-08-20 19:57:55 -0300

    Adicionando informações ao edital

--- concurso/EDITAIS/EM ESTUDO/TST - TRIBUNAL SUPERIOR DO TRABALHO/2017_FCC_ANALISTA_DE_SISTEMAS/conteudo_edital.cc
+++ concurso/EDITAIS/EM ESTUDO/TST - TRIBUNAL SUPERIOR DO TRABALHO/2017_FCC_ANALISTA_DE_SISTEMAS/conteudo_edital.cc
@@ -1,17 +1,20 @@
-## CONHECIMENTOS BÁSICOS
+PRECISO TIRAR 9!!!!!
+
+## CONHECIMENTOS BÁSICOS (OUTUBRO!!!!!)
 PARA TODOS OS CARGOS DE ANALISTA JUDICIÁRIO,
 EXCETO PARA O CARGO DE ANALISTA JUDICIÁRIO – ÁREA APOIO ESPECIALIZADO – ESPECIALIDADE TAQUIGRAFIA
 
-# Língua Portuguesa '1 PASSADA SEM COMPLETAR'
-41% Interpretação de texto. Argumentação. Pressupostos e subentendidos. 
-15% Equivalência e transformação de estruturas. Redação.
-7%  Concordância nominal e verbal. 
-7%  Pontuação. 
+# Língua Portuguesa '1 PASSADA SEM COMPLETAR' 
+41% Interpretação de texto. Argumentação. Pressupostos e subentendidos. 													'5.474 - FCC - Português - Interpretação de Textos'
+15% Equivalência e transformação de estruturas. Redação.	 																'34 - FCC - Português - Significação Contextual de Palavras e Expressões. Sinônimos e Antônimos. - Redação - Reescritura de texto'
+7%  Concordância nominal e verbal. 																							'9  - FCC - Português - Concordância verbal, Concordância nominal'
+7%  Pontuação. 																												'6  - FCC - Português - Pontuação'
 'PARE AQUI - 70% CONTEUDO - TERMINE TODOS OS EXERCICIOS DE CIMA - LER MATERIAL COMPLEMENTAR'
 6%  Tempos, modos e vozes verbais. 
 5%  Classes de palavras.
 4%  Articulação do texto: coesão e coerência. 
 4%  Flexão nominal e verbal.
+'PARE AQUI - 89% CONTEUDO - TERMINE TODOS OS EXERCICIOS DE CIMA - LER MATERIAL COMPLEMENTAR'
 3%  Termos da oração. Processos de coordenação e subordinação. 
 3%  Regência nominal e verbal.
 3%  Ocorrência da Crase.
@@ -23,10 +26,11 @@ Discurso direto e indireto.
 47% Inclusão, direitos e garantias legais e constitucionais das pessoas com deficiência
 	Resolução CNJ no 230/2016 - art. 19; Lei no 13.146/2015; Lei no 11.126/2005; Constituição Federal.
 33% Normas gerais e critérios básicos para a promoção da acessibilidade das pessoas portadoras de deficiência ou com mobilidade reduzida 
-	Lei no 10.098/2000;Decreto 5.296/2004. 
+	Lei no 10.098/2000;Decreto 5.296/2004.
 'PARE AQUI - 80% CONTEUDO - TERMINE TODOS OS EXERCICIOS DE CIMA - LER MATERIAL COMPLEMENTAR'
 10% Prioridade de atendimento às pessoas portadoras de deficiência 
 	Lei no 10.048/2000;Decreto 5.296/2004. 
+'PARE AQUI - 90% CONTEUDO - TERMINE TODOS OS EXERCICIOS DE CIMA - LER MATERIAL COMPLEMENTAR'
 10% Normas de apoio às pessoas portadoras de deficiência e sua integração social 
 	Lei no 7.853/1989;Decreto 3.298/1999.
 Direitos no sistema de transporte coletivo 
@@ -40,30 +44,32 @@ Aprovado pela Resolução Administrativa no 1.295/2008
 	com alterações dos 
 	Atos Regimentais 1/2011;2/2011;3/2012
 	Emendas Regimentais 1/2011;2/2011;3/2012.
+'PARE AQUI - 100% CONTEUDO - TERMINE TODOS OS EXERCICIOS DE CIMA - LER MATERIAL COMPLEMENTAR'
 
 ## CONHECIMENTOS ESPECÍFICOS
 C03 - ANALISTA JUDICIÁRIO – ÁREA APOIO ESPECIALIZADO – ESPECIALIDADE ANÁLISE DE SISTEMAS
 
 # Bancos de Dados '3 PASSADAS SEM COMPLETAR'
-31% SQL. -->> FAZER AQUI
-19% Modelo Entidade-Relacionamento.
-16% Oracle PL/SQL.
-11% Normalização de Banco de Dados.
+3 - 31% SQL. 																												'207 - FCC - Banco de Dados - SQL'
+19% Modelo Entidade-Relacionamento. 																						'216 - FCC - Banco de Dados'MER Entidade Relacionamento
+16% Oracle PL/SQL. 																											'83  - FCC - Banco de Dados - PL-SQL'
+11% Normalização de Banco de Dados. 																						'38  - FCC - Banco de Dados - Formas normais'
 'PARE AQUI - 77% CONTEUDO - TERMINE TODOS OS EXERCICIOS DE CIMA - LER MATERIAL COMPLEMENTAR'
 10% Características de um SGBD.
 7%  SQL Server.
+'PARE AQUI - 94% CONTEUDO - TERMINE TODOS OS EXERCICIOS DE CIMA - LER MATERIAL COMPLEMENTAR'
 3%  Modelo relacional.
 3%  Modelagem de bancos de dados.
 Princípios sobre administração de bancos de dados.
 
 # Engenharia e Desenvolvimento de Software '3 PASSADAS SEM COMPLETAR'
-3 - 20% Aspectos gerais das linguagens PHP, HTML5, XML, Java SE, Java EE, JUnit.
-3 - 17% Análise e projeto orientados a objetos (UML).
-8%  Princípios sobre qualidade de software: modelo MPS.BR e CMMI-DEV versão 1.3.
-7%  Processos de software.
-7%  Estruturas de dados e de controle de fluxo.
-7%  Conceitos sobre orientação a objetos (classes, atributos, objetos, métodos, herança, interfaces e outras hierarquias).
-5%  Engenharia de requisitos.
+5 - 20% Aspectos gerais das linguagens PHP, HTML5, XML, Java SE, Java EE, JUnit.											'237 - FCC - Programação - PHP - HTML - XML - Java SE - JEE - JUnit'
+3 - 17% Análise e projeto orientados a objetos (UML). 																		'235 - FCC - Engenharia de Software - UML'
+8%  Princípios sobre qualidade de software: modelo MPS.BR e CMMI-DEV versão 1.3. 											'138 - FCC - Governança de TI - MPS.BR - CMMI'
+7%  Processos de software. 																									'26  - FCC - Governança de TI - ISO-IEC 12207 '
+7%  Estruturas de dados e de controle de fluxo. 																			'27  - FCC - Algoritmos e Estrutura de Dados - Estrutura de dados - Estruturas de controle e repetição'
+7%  Conceitos sobre orientação a objetos (classes, atributos, objetos, métodos, herança, interfaces e outras hierarquias).  '118 - FCC - Engenharia de Software - Orientação a Objetos'
+5%  Engenharia de requisitos. 																								'67  - FCC - Engenharia de Software - Engenharia de Requisitos'
 'PARE AQUI - 71% CONTEUDO - TERMINE TODOS OS EXERCICIOS DE CIMA - LER MATERIAL COMPLEMENTAR'
 4%  Métodos ágeis (eXtreme Programming – XP, Scrum, Kanban).
 3%  Javascript (ECMAScript 6), bootstrap. jquery.
@@ -72,6 +78,7 @@ Princípios sobre administração de bancos de dados.
 2%  Ciclo de vida de sistemas.
 2%  CSS3. 
 2%  Modelagem funcional.
+'PARE AQUI - 90% CONTEUDO - TERMINE TODOS OS EXERCICIOS DE CIMA - LER MATERIAL COMPLEMENTAR'
 2%  Padrões de Projeto (design patterns).
 1%  Princípios de engenharia de software.
 1%  Modelos de desenvolvimento de sistemas.
@@ -87,21 +94,23 @@ Conceitos de linguagens estruturadas.
 Conceitos sobre desenvolvimento Web e cliente/servidor.
 
 # Sistemas de apoio à decisão '3 PASSADAS SEM COMPLETAR'
-44% Arquitetura OLAP.
-18% Data Mining.
-14% Data Warehouse.
+44% Arquitetura OLAP. 																										'31 - FCC - Banco de Dados - OLAP (On-line Analytical Processing)'
+18% Data Mining. 																											'16 - FCC - Banco de Dados - Data Mining'
+14% Data Warehouse. 																										'53 - FCC - Banco de Dados - DW - Data Warehouse'
 'PARE AQUI - 76% CONTEUDO - TERMINE TODOS OS EXERCICIOS DE CIMA - LER MATERIAL COMPLEMENTAR'
 13% ERP.
+'PARE AQUI - 89% CONTEUDO - TERMINE TODOS OS EXERCICIOS DE CIMA - LER MATERIAL COMPLEMENTAR'
 6%  Business Intelligence: Conceitos Princípios e Processos.
 4%  ETL.
 
 # Governança e Gestão de TI '3 PASSADAS SEM COMPLETAR'
-66% Gestão de serviços de TI com ITIL v3 Edição 2011 - Estrutura, objetivos e conceitos gerais.
-8%  Governança e Gestão de TI com COBIT 5 - Estrutura, objetivos e conceitos gerais.
+66% Gestão de serviços de TI com ITIL v3 Edição 2011 - Estrutura, objetivos e conceitos gerais. 							'144 - FCC - Governança de TI - ITIL v. 3'
+8%  Governança e Gestão de TI com COBIT 5 - Estrutura, objetivos e conceitos gerais. 										'32  - FCC - Governança de TI - COBIT v. 5.0'
 'PARE AQUI - 74% CONTEUDO - TERMINE TODOS OS EXERCICIOS DE CIMA - LER MATERIAL COMPLEMENTAR'
 8%  Planejamento estratégico de TI – PETI.
 6%  Gestão de projetos com PMBOK 5a Edição.
 5%  Gestão de riscos. Programa de gestão de riscos. Processo de gestão de riscos. Métodos de análise de riscos.
+'PARE AQUI - 93% CONTEUDO - TERMINE TODOS OS EXERCICIOS DE CIMA - LER MATERIAL COMPLEMENTAR'
 2%  Estrutura organizacional e responsabilidades da TI. Responsabilidade e papéis da TI. Segregação de funções de TI. Controles de segregação de funções.
 1%  Terceirização da TI.
 1%  Alinhamento estratégico entre áreas de TI e negócios. Políticas e procedimentos.
@@ -110,5 +119,11 @@ Conceitos sobre desenvolvimento Web e cliente/servidor.
 Práticas de gestão da TI.
 
 ## ESTRATEGIA DE RESOLUÇÂO
-- DAR ÊNFASE EM CONTEÚDO PORCENTUAL DE ACORDO COM DIFICULDADE
-- COLOCAR PALAVRAS CHAVE NAS BUSCAS!!!
+- NÃO DÁ PRA ESTUDAR TUDO, FAZ EXERCICIO
+- SE CERTEZA
+	PASSA 
+  SENAO
+  	ANOTA E PESQUISA
+- VAI DAR TUDO CERTO
+- ESTUDAR NO ÔNIBUS SÓ OS CONTEUDOS QUE VOCÊ NÃO TEM IDEIA!!!!
+- FAZENDO EXERCICIO VOCE VAI SABER QUAIS NÃO TEM IDEIA!!!!	
