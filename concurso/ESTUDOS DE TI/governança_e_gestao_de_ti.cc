1a5fe25bbfc577cfafea48af19fcdaa4cb31c2cd
gabrielmirandat <gabrielmirandatt@gmail.com>
2017-08-24 22:57:01 -0300

    Adicionado estudo de governança

---
+++ concurso/ESTUDOS DE TI/governança_e_gestao_de_ti.cc
@@ -0,0 +1,355 @@
+//-- 66% Gestão de serviços de TI com ITIL v3 Edição 2011 - Estrutura, objetivos e conceitos gerais
+
+- ITIL'Biblioteca de Infraestrutura de Tecnologia da Informação'
+	- conjunto de publicações sobre melhores práticas para gerenciamento de serviços de TI
+	- informações sobre processos, funções e outras habilidades
+	- elementos para entregar serviços de TI com qualidade
+	- descreve práticas testadas e validadas por várias organizações do mundo
+	- ITIL v3 traz a abordagem da ITIL reforçada com a estrutura do ciclo de vida de serviço
+	- ITIL V3 2011 é a nova publicação seguindo a atualização
+	- considerada como a mais reconhecida referência de práticas de gerenciamento de serviços de TI do mundo
+	- a nova versão é totalmente diferente da primeira versão
+	
+	- descrito em 5 livros
+		- A ITIL sugere que as atividades de gerenciamento de serviços sejam estruturadas com base no 'ciclo de vida do serviço'
+		- Esse ciclo considera a vida do serviço desde a sua concepção até a sua descontinuação
+		- Cada um dos cinco principais livros da biblioteca se 'refere a um estágio específico do ciclo de vida do serviço'
+			- 1. Estratégia de Serviço
+			- 2. Desenho de Serviço
+			- 3. Transição de Serviço
+			- 4. Operação de Serviço
+			- 5. Melhoria continuada de serviço 
+
+		- 1. Estratégia de Serviço
+			- O objetivo é definir a direção estratégica dos serviços de TI
+			- quem são os seus clientes e quais serviços serão disponibilizados pra eles
+			- pensar de uma maneira mais estratégica
+				- melhor uso dos serviços pra beneficiar a organização?
+				- diferenciar de outros competidores?
+				- criar valor para nossos clientes?
+			- gerenciamento de serviços como um ativo estratégico e entrega sempre focada em suportar as necessidades de negocio
+			- Duas áreas
+				- uso do gerenciamento de serviços como um ativo estratégico
+				- processos que vão habilitar a organização de TI a gerenciar os seus serviços de TI através do cilo de vida de serviço
+			- Ao definir estratégia, estabelecer políticas para facilitar a implementação da estratégia e atingir os resultados propostos
+			- estratégia de serviço ainda inclui atividades para aprovar os serviços e adquirir o financiamento e recursos para desenvolve-lo
+			- Todas as questões detalhadas nos seguintes processos
+				- Gerenciamento estratégico para serviços de TI
+				- Gerenciamento de relacionamento do negócio
+				- Gerenciamento do portfólio de serviço
+				- Gerenciamento financeiro para serviços de TI
+				- Gerenciamento da demanda
+		
+		- 2. Desenho de Serviço
+			- O objetivo é desenhar um novo serviço ou modificar um já existente visando sua entrada no ambiente produtivo
+			- momento onde a ideia de um novo serviço ou a modificação de um serviço existente vai para o papel
+			- a partir dos requisitos anteriores, são construídos planos, desenhos e estimativas de recursos
+			- Incorpora os seguintes processos 
+				- Coordenação do desenho 
+				- Gerenciamento de nível de serviço 
+				- Gerenciamento do catalogo de serviço
+				- Gerenciamento de fornecedores 
+				- Gerenciamento da disponibilidade e capacidade 
+				- Gerenciamento da continuidade de serviços de TI
+				- Gerenciamento da Segurança da Informação 
+			- Incorpora os seguintes atos
+				- Como projetar a infraestrutura de forma que se mantenha segura e tolerante a falhas.
+				- Como identificar e gerenciar riscos e criar um mapeamento completo dos riscos possíveis.
+				- Quais documentos devem ser usados para registrar os planos, políticas, arquiteturas e treinamento da equipe.
+				- Como alertar a Central de Serviços com antecedência sobre novos serviços e como preparar e treinar o pessoal de suporte.
+				- Como contribuir para a melhoria continuada do serviço assegurando que está sendo implantada qualidade do serviço no ambiente de produção.
+
+		- 3. Transição de Serviço
+			- O objetivo é ajudar a organização a planejar e gerenciar mudanças em serviços e implementar liberações no ambiente produtivo
+			- Por exemplo, instalar ou configurar um software, hardware e qualquer outro componente ou documentação associada
+			- garantir que os requisitos da Estratégia, incluídos no Desenho, sejam efetivamente realizados na Operação ao mesmo tempo controlando 
+			  os riscos de falhas e interrupções
+			- serviços são tirados do papel e de fato materializados, em algo “consumível”
+			- Os seguintes processos fazem parte desse estágio:
+				- Planejamento e suporte da transição 
+				- Gerenciamento de mudanças
+				- Gerenciamento da configuração e ativos de serviço
+				- Gerenciamento de liberação e implantação 
+				- Gerenciamento do conhecimento
+
+		- 4. Operação de Serviço
+			- O objetivo é coordenar e executar as atividades e processos necessários pra entregar os serviços aos clientes e usuários do negocio, 
+			  gerenciando os serviços nos níveis acordados.
+			- cobre o gerenciamento de rotina da tecnologia usada pra entregar ou suportar os serviços
+			- O momento em que os serviços de TI passam a ser consumidos pelos clientes e usuários
+			- aqui o valor do serviço é percebido!
+			- Os processos deste estagio do ciclo de vida incluem:
+				- Gerenciamento de eventos
+				- Gerenciamento de incidentes
+				- Cumprimento de requisição 
+				- Gerenciamento de problemas 
+				- Gerenciamento de acesso
+
+		- 5. Melhoria continuada de serviço
+			- O objetivo é alinhar e realinhar continuamente os serviços de TI de acordo com as necessidades do cliente, identificando e 
+			  implementando melhorias aos serviços de TI que suportam os processos de negocio
+			- Atividades
+				- Identificar, ou ajudar a identificar oportunidades de melhoria
+				- Priorizar as melhorias identificadas
+				- Definir e executar, ou ajudar a definir e executar, iniciativas de melhoria.
+			- não diz respeito a um projeto de melhoria, mas a um número de iniciativas ou projetos recorrentes que visam melhorar um ou mais aspectos dos serviços de TI
+
+
+	Exemplos ITIL v3
+		- solução de contorno
+			- 'Ação de reiniciar um servidor'
+		- evento
+			- 'O antivírus está desatualizado'
+		- incidente
+			- 'Todas as portas USB pararam de funcionar'
+		- alerta
+			- 'A memória do servidor de Banco de Dados ultrapassou 80% da taxa de ocupação'
+
+	Situações
+		- Se chamado aberto em relação a um incidente
+			- teve o atendimento de 1° nível na Central de Serviços
+			- após ser inicialmente diagnosticado, o técnico não conseguiu aplicar uma solução de contorno ou fornecer uma resolução
+		- 'Solução': incidente deve ser escalado, passado para profissional com mais experiência ou profissionais de nível gerencial
+
+	Auditoria de TI
+		- É uma atividade independente que tem como missão o gerenciamento de risco operacional envolvido e avaliar 
+		  a adequação das tecnologias e sistemas de informação utilizados na organização através da revisão e avaliação
+
+
+//-- 8%  Governança e Gestão de TI com COBIT 5 - Estrutura, objetivos e conceitos gerais
+
+- COBIT v5
+	- Governança'O sistema pelo qual o uso atual e futuro da TI é dirigido e controlado'
+		- avaliação e direção do uso da TI
+		- suporte à organização no alcance de objetivos estratégicos
+		- monitorar uso da TI para realizar planos
+		- inclue a estratégia e políticas para uso de TI dentro de uma organização
+
+		- governar a TI por meio de 3 tarefas principais
+			- 'Avaliar' o uso atual e futuro da TI 													  'checar se uso de determinada tecnologia é relevante'
+				- inclui avaliar estratégias, propostas, arranjos de fornecimento interno ou externo
+				- inclui avaliar pressoes externas e internas, tais como
+					- mudanças tecnologicas, tendências economicas, influências políticas
+			- 'Dirigir' a preparação e a implementação de planos p/ atender objetivos do negócio      'preparar plano para chegar a objetivos'
+				- inclui atribuir responsabilidades para preparação e implementação de planos
+				- os planos devem estabelecer o direcionamento de investimentos nos projetos/operações
+			- 'Monitorar' cumprimento das políticas em relação ao plano 							   'monitorar política que implementa os plano'
+				- monitorar o desempenho com sistemas de mensuração
+				- garantir que o desempenho está de acordo com os planos definidos
+				- ainda checa se objetivos estão em conformidade com as obrigações dentro e fora do ambiente de trabalho
+
+		- A governança é de responsabilidade da alta administração (board), controla a gestão
+		- A gestão é de responsabilidade dos gestores de TI, controla tarefas operacionais
+
+		- COBIT 5 é um framework de governança e gestão corporativa de TI.
+			- faz a integração do conteúdo dos principais frameworks publicados pelo ISACA, empresa em que foi desenvolvido
+				- 'COBIT 4.1'
+				- 'Val IT', conjunto de processos e melhores práticas
+				- 'Risk IT', auxiliar no gerenciamento de riscos
+				- 'Business Model for Information Security (BMIS)', gestão de segurança da informação
+				- 'IT Assurance Framework (ITAF)', concepção, realização e relatório de auditoria de TI
+				- 'Taking Governance Forward (TGF)'
+				- 'Board Briefing on IT Governance 2nd Edition', descrição abrangente dos conceitos de governança de TI para educar o board e a gerência executiva
+			- alinha-se com outros padrões e modelos do mercado, tais como ITIL, TOGAF, PMBOK, PRINCE2, COSO, ISO
+
+			- auxilia organizações a 
+				- criação de valor para TI
+				- equilíbrio entre a realização de benefícios e a otimização dos níveis de risco e o uso de recursos
+
+			- objetivos
+				- auxilia as organizações a otimizar o valor gerado pela TI
+				- permitir que a TI seja governada e gerenciada por inteiro
+				- criar uma linguagem comum entre TI e negócios
+
+			- pra que foi criado
+				- fornece a próxima geração de orientações da ISACA sobre governança corporativa e gestão de TI
+				- mais de 15 anos de uso e aplicação prática por muitos na TI
+				- Permitir que mais partes interessadas falem sobre o que eles esperam da tecnologia da informação e tecnologias relacionadas, prioridades e valor esperado
+				- dependência cada vez maior para o sucesso da organização
+				- Tratar a quantidade de informação
+				- Administrar TI cada vez mais pervasiva
+				- Cobrir o negócio por inteiro
+
+			- compoe 
+				- Sumário Executivo
+				- Componentes e estruturas
+				- 5 princípios, descritos cada um em um capítulo distinto
+				- Visão dos 7 habilitadores e suas dimensões
+				- Cascata de objetivos (COBIT 5 Goals Cascade)
+				- Modelo de Referência de Processos
+				- Introdução ao Guia de Implementação
+				- Modelo de Capacidade de Processos
+
+			- Área 
+				- Governança
+					- 5 processos 
+					- Avaliar, Dirigir e Monitorar (Evaluate, Direct and Monitor - EDM)
+				- Gestão 
+					- 32 processos 
+					- Alinhar, Planejar e Organizar (Align, Plan and Organise - APO) ;
+					- Construir, Adquirir e Implementar (Build, Acquire and Implement - BAI) ;
+					- Entregar, Serviços e Suporte (Deliver, Service and Support - DSS)
+					- Monitorar, Avaliar e Analisar (Monitor, Evaluate and Assess - MEA)
+
+			- COBIT 5 é
+				- Focado em governança corporativa de TI
+				- Deixa clara a distinção entre governança e gestão
+				- Fundamentado em 5 princípios de governança corporativa de TI
+				- Baseado em um conjunto holístico de 7 enablers (ou habilitadores)
+				- Possui 37 processos de TI divididos em domínios de processo de governança e de gestão
+
+			- Evolução
+				- COBIT1'auditoria e controles de TI'
+				- COBIT3'orientações para a gestão de TI'
+				- COBIT4'framework de governança de TI'
+				- COBIT5'integrador de governança e gestão de TI corporativa'	
+
+			- 'Princípios'
+				1. Atender as necessidades dos stakeholders (partes interessadas)
+				2. Cobrir a organização de ponta a ponta
+				3. Aplicar um framework (modelo) único e integrado
+				4. Permitir uma abordagem completa
+				5. Distinguir a governança da gestão
+
+				1. Atender as necessidades dos stakeholders (partes interessadas)
+					- As organizações existem para criar valor para as partes interessadas (stakeholders)
+					- acionistas, auditores, fornecedores, consultores, alta administração, etc
+					- Consequentemente, qualquer organização terá a criação de valor como um objetivo da governança
+					- criação de valor'Obtenção de benefícios por meio da otimização do uso de recursos e dos riscos a um nível aceitável'
+						- Realização de benefícios'financeiros ou serviço publico'
+						- Otimização do risco
+						- Otimização dos recursos
+					- É o alinhamento estratégico entre TI e negócio
+
+					Para cada decisão de governança, as seguintes questões podem e devem ser feitas
+						- Quem recebe os benefícios?
+						- Quem assume os riscos?
+						- Quais são os recursos necessários?
+
+					Cascata de Objetivos do COBIT 5'auxilia em como empregar os habilitadores para alcançar os objetivos corporativos de forma mais concreta'
+						- desdobrar os direcionadores (drives) e as necessidades dos stakeholders em objetivos corporativos
+						- desdobrar os objetivos corporativos em objetivos de TI
+						- desdobrar os objetivos de TI em objetivos para os habilitadores
+
+					No COBIT 5, os objetivos de TI são desdobrados em objetivos de habilitadores, sendo Processos um dos habilitadores
+
+				2. Cobrir a organização de ponta a ponta
+					- Integra a governança corporativa de TI dentro da governança corporativa da organização;
+					- Cobre todas as funções e processos necessários para regular e controlar as informações
+					- Não foca apenas nas funções de TI, mas informação e tecnologia como ativos
+
+					- os gestores de negócio têm a responsabilidade de tratar a TI como um ativo estratégico
+
+					- Componentes
+						- Habilitadores da governança'recursos como princípios, estruturas, processos e práticas'
+						- Escopo da governança'toda a organização ou só uma parte'
+						- Papéis, Atividades e Relacionamentos'quem está envolvido com governança, como estão envolvidos, o que fazem e como interagem'
+
+				3. Aplicar um framework (modelo) único e integrado
+					- COBIT 5 é uma estrutura única e integrada
+					- permite à organização utilizar o COBIT 5 como um integrador dos frameworks de governança e de gestão
+					
+					- família de produtos COBIT 5
+						- COBIT 5 (o framework)
+						- Guia de habilitadores do COBIT 5
+							- COBIT 5: Habilitador Processos (Enabling Processes)
+							- COBIT 5: Habilitador Informação (Enabling Information)
+							- Outros guias habilitadores
+						- Guias profissionais do COBIT 5
+							- COBIT 5 Implementação
+							- COBIT 5 para Segurança da Informação
+							- COBIT 5 para Garantia (Assurance)
+							- COBIT 5 para Risco
+							- Outros guias profissionais
+						- Um ambiente colaborativo on-line, que é disponibilizado para apoiar o uso do COBIT 5
+
+				4. Permitir uma abordagem completa
+					- COBIT 5 define um conjunto de 7 habilitadores
+					
+					- 'Habilitadores' Fatores que, individual e coletivamente, influenciam o funcionamento da governança e gestão corporativas de TI 
+					- são orientados pela cascata de objetivos
+					- objetivos de TI em níveis mais alto definem o que os diferentes habilitadores deverão alcançar
+						1. Princípios, políticas e frameworks'veículos que traduzem o comportamento desejado para gestão diária'
+						2. Processos'conjunto organizado de práticas e atividades para atingir determinados objetivos'
+						3. Estruturas organizacionais'entidades-chave responsáveis pela tomada de decisão'
+						4. Cultura, ética e comportamento'fator de sucesso nas atividades de governança e gestão'
+						5. Informação'todas as informações produzidas e utilizadas, sendo imprescindível para manter a organização em funcionamento e bem governada'
+						6. Serviços, infraestrutura e aplicações'infraestrutura, tecnologia e aplicações que fornecem o serviço de TI'
+						7. Pessoas, habilidades e competências'necessárias para que as atividades sejam executadas com sucesso'
+
+					- Dimensões comuns para habilitadores
+						- Partes interessadas
+							- cada habilitador tem interessados que executam papel ativo ou têm interesse na execução
+						- Objetivos
+							- cada habilitador têm objetivos e criam valor pela realização destes objetivos
+							- divididos em qualidade intrínseca, qualidade contextual e acessibilidade e segurança
+						- Ciclo de vida
+							- desde sua criação, passando por sua vida útil/operacional até chegar ao descarte
+								- Planejar (inclui o desenvolvimento de conceitos e seleção de conceitos)
+								- Projetar
+								- Construir/adquirir/criar/implementar
+								- Utilizar/operar
+								- Avaliar/monitor
+								- Atualizar/eliminar
+						- Boas práticas
+							- Boas práticas apoiam a realização dos objetivos do habilitador
+
+				5. Distinguir a governança da gestão
+					- Essas duas áreas abrangem diferentes tipos de atividades
+					- exigem diferentes estruturas organizacionais e servem a propósitos diferentes
+
+					Governança
+						- A governança garante que as necessidades, as condições e as opções das partes interessadas sejam avaliadas a fim de 
+						- determinar os objetivos corporativos acordados e equilibrados; 
+						- define a direção por meio de priorização e tomada de decisão; e 
+						- provê monitoramento de desempenho e conformidade com relação aos objetivos estabelecidos.
+						- A governança é de responsabilidade do Conselho de Administração, sob a liderança do presidente
+
+					Gestão
+						- A gestão consiste em planejar, construir, executar e monitorar atividades alinhadas com a direção estratégica estabelecida pela governança para
+						- atingir os objetivos corporativos.
+						- A gestão é da responsabilidade da gerência executiva, sob a liderança do chefe diretor executivo (CEO)
+
+
+			- MODELO DE REFERÊNCIA DE PROCESSOS
+				- 37 processos de TI em duas principais áreas de atividade 
+				- governança e gestão
+				- Governança '5'
+					- Avaliar 
+					- Dirigir 
+					- Monitorar 
+				- Gestão 
+					- Planejar  (APO - Align, Plan and Organise)         '13'
+						- estratégia e táticas de TI
+						- arquitetura corporativa
+						- inovação e gerenciamento de portfólio
+						- orçamento, qualidade, riscos e segurança
+					- Construir (BAI - Build, Acquire and Implement)     '10'
+						- torna a estratégia de TI concreta
+						- identificando os requisitos para a TI
+						- gerenciando o programa de investimentos em TI e projetos associados
+						- gerenciamento da disponibilidade e capacidade
+						- mudança organizacional
+						- gerenciamento de mudanças (TI)
+						- aceite e transição
+						- gerenciamento de ativos, configuração e conhecimento
+					- Entregar  (DSS - Deliver, Service and Support)     '6'
+						- à entrega dos serviços de TI necessários para atender aos planos táticos e estratégicos
+						- processos para gerenciar operações, requisições de serviços e incidentes
+						- gerenciamento de problemas, continuidade, serviços de segurança e controle de processos de negócio
+					- Monitorar (MEA - Monitor, Evaluate and Assess) 	 '3'
+						- monitorar o desempenho dos processos
+						- avaliando a conformidade com os objetivos e com os requisitos externos
+
+			- Para cada processo
+				- Identificação
+				- Descrição 
+				- Propósito 
+				- Informação de objetivos em cascata
+				- Objetivos de processos e métricas
+				- Matriz RACI
+				- Descrição detalhada de práticas de processo
+				- Guias relacionados
+
+			- uma capacidade de processo nível 3-Processo Estabelecido exige que a definição e os atributos de implantação do 
+			  processo sejam amplamente atingidos depois que a capacidade dos atributos de processo do nível 2-Processo Gerenciado forem atingidos
