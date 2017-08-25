a13f53cd4932cc9a73d784a5e34b4620f609a94d
gabrielmirandat <gabrielmirandatt@gmail.com>
2017-08-24 22:56:13 -0300

    Adicionado estudo de cobit

--- concurso/EDITAIS/EM ESTUDO/TST - TRIBUNAL SUPERIOR DO TRABALHO/2017_FCC_ANALISTA_DE_SISTEMAS/conteudo_edital.cc
+++ concurso/EDITAIS/EM ESTUDO/TST - TRIBUNAL SUPERIOR DO TRABALHO/2017_FCC_ANALISTA_DE_SISTEMAS/conteudo_edital.cc
@@ -1,27 +1,31 @@
 PRECISO TIRAR 9!!!!!
 
+
+//-- --//
 ## CONHECIMENTOS BÁSICOS (OUTUBRO!!!!!)
 PARA TODOS OS CARGOS DE ANALISTA JUDICIÁRIO,
 EXCETO PARA O CARGO DE ANALISTA JUDICIÁRIO – ÁREA APOIO ESPECIALIZADO – ESPECIALIDADE TAQUIGRAFIA
 
+//--
 # Língua Portuguesa '1 PASSADA SEM COMPLETAR' 
 41% Interpretação de texto. Argumentação. Pressupostos e subentendidos. 													'5.474 - FCC - Português - Interpretação de Textos'
 15% Equivalência e transformação de estruturas. Redação.	 																'34 - FCC - Português - Significação Contextual de Palavras e Expressões. Sinônimos e Antônimos. - Redação - Reescritura de texto'
 7%  Concordância nominal e verbal. 																							'9  - FCC - Português - Concordância verbal, Concordância nominal'
 7%  Pontuação. 																												'6  - FCC - Português - Pontuação'
 'PARE AQUI - 70% CONTEUDO - TERMINE TODOS OS EXERCICIOS DE CIMA - LER MATERIAL COMPLEMENTAR'
-6%  Tempos, modos e vozes verbais. 
+6%  Tempos, modos e vozes verbais.  																						'233 - FCC - Português - Flexão verbal de tempo (presente, pretérito, futuro) ExcluirFlexão verbal de modo (indicativo, subjuntivo, imperativo) ExcluirFlexão de voz (ativa, passiva, reflexiva)'
 5%  Classes de palavras.
-4%  Articulação do texto: coesão e coerência. 
+4%  Articulação do texto: coesão e coerência.  																				'20  - FCC - Português - Coesão e coerência'
 4%  Flexão nominal e verbal.
 'PARE AQUI - 89% CONTEUDO - TERMINE TODOS OS EXERCICIOS DE CIMA - LER MATERIAL COMPLEMENTAR'
-3%  Termos da oração. Processos de coordenação e subordinação. 
-3%  Regência nominal e verbal.
-3%  Ocorrência da Crase.
-2%  Ortografia e acentuação.
+3%  Termos da oração. Processos de coordenação e subordinação. 																'5   - FCC - Português - Termos essenciais da oração: Sujeito e Predicado - Termos integrantes da oração: Objeto direto, Objeto indireto, Complemento nominal, Agente da Passiva - Termos integrantes da oração: predicativo do sujeito e predicativo do objeto - Orações coordenadas assindéticas - Orações coordenadas sindéticas: Aditivas, Adversativas, Alternativas, Conclusivas... - Orações subordinadas adjetivas: Restritivas, Explicativas - Orações subordinadas substantivas: Subjetivas, Objetivas diretas, Objetivas indiretas... - Orações subordinadas adverbiais: Causal, Comparativa, Consecutiva, Concessiva, Condicional... - Orações subordinadas reduzidas' 
+3%  Regência nominal e verbal. 																								'7   - FCC - Português - Regência'
+3%  Ocorrência da Crase. 																									'2   - FCC - Português - Crase'
+2%  Ortografia e acentuação. 																								'6   - FCC - Português - Ortografia - Acentuação gráfica: Proparoxítonas, Paroxítonas, Oxítonas e Hiatos - Acentuação gráfica: acento diferencial'
 Níveis de linguagem. 
-Discurso direto e indireto. 
+Discurso direto e indireto.  																								'2   - FCC - Português - Tipologia Textual e Tipos de Discurso: direto, indireto e indireto livre.'
 
+//-- QUESTOES DO MATERIAL LIDO
 # Noções sobre Direitos das Pessoas com Deficiência '1 PASSADA SEM COMPLETAR'
 47% Inclusão, direitos e garantias legais e constitucionais das pessoas com deficiência
 	Resolução CNJ no 230/2016 - art. 19; Lei no 13.146/2015; Lei no 11.126/2005; Constituição Federal.
@@ -38,7 +42,7 @@ Direitos no sistema de transporte coletivo
 Símbolo de identificação de pessoas portadoras de deficiência auditiva
 	Lei no 8.160/1991. 
 
-
+//-- QUESTOES DO MATERIAL LIDO
 # Regimento Interno do TST '1 PASSADA SEM COMPLETAR'
 Aprovado pela Resolução Administrativa no 1.295/2008
 	com alterações dos 
@@ -46,84 +50,87 @@ Aprovado pela Resolução Administrativa no 1.295/2008
 	Emendas Regimentais 1/2011;2/2011;3/2012.
 'PARE AQUI - 100% CONTEUDO - TERMINE TODOS OS EXERCICIOS DE CIMA - LER MATERIAL COMPLEMENTAR'
 
+
+//-- --//
 ## CONHECIMENTOS ESPECÍFICOS
 C03 - ANALISTA JUDICIÁRIO – ÁREA APOIO ESPECIALIZADO – ESPECIALIDADE ANÁLISE DE SISTEMAS
 
+//--
 # Bancos de Dados '3 PASSADAS SEM COMPLETAR'
-3 - 31% SQL. 																												'207 - FCC - Banco de Dados - SQL'
-19% Modelo Entidade-Relacionamento. 																						'216 - FCC - Banco de Dados'MER Entidade Relacionamento
-16% Oracle PL/SQL. 																											'83  - FCC - Banco de Dados - PL-SQL'
-11% Normalização de Banco de Dados. 																						'38  - FCC - Banco de Dados - Formas normais'
+*3 - 31% SQL. 																												'207 - FCC - Banco de Dados - SQL'
+*5 - 19% Modelo Entidade-Relacionamento. 																					'216 - FCC - Banco de Dados'MER Entidade Relacionamento
+16% Oracle PL/SQL. 																											'164 - FCC - Banco de Dados - Oracle - PL-SQL'
+*11% Normalização de Banco de Dados. 																						'38  - FCC - Banco de Dados - Formas normais'
 'PARE AQUI - 77% CONTEUDO - TERMINE TODOS OS EXERCICIOS DE CIMA - LER MATERIAL COMPLEMENTAR'
-10% Características de um SGBD.
-7%  SQL Server.
+*10% Características de um SGBD. 																							'32  - FCC - Banco de Dados - SGBD - Sistema de Gerenciamento de Banco de Dados'
+7%  SQL Server. 																											'41  - FCC - Banco de Dados - SQL Server '
 'PARE AQUI - 94% CONTEUDO - TERMINE TODOS OS EXERCICIOS DE CIMA - LER MATERIAL COMPLEMENTAR'
-3%  Modelo relacional.
-3%  Modelagem de bancos de dados.
-Princípios sobre administração de bancos de dados.
+*3%  Modelo relacional. 																									'28  - FCC  - Banco de Dados - Modelo relacional'
+*3%  Modelagem de bancos de dados. 																							'29  - FCC  - Banco de Dados - Modelagem de dados'
+*Princípios sobre administração de bancos de dados. 																		'13  - FCC  - Banco de Dados - Administração de banco de dados'
 
+//--
 # Engenharia e Desenvolvimento de Software '3 PASSADAS SEM COMPLETAR'
 5 - 20% Aspectos gerais das linguagens PHP, HTML5, XML, Java SE, Java EE, JUnit.											'237 - FCC - Programação - PHP - HTML - XML - Java SE - JEE - JUnit'
 3 - 17% Análise e projeto orientados a objetos (UML). 																		'235 - FCC - Engenharia de Software - UML'
-8%  Princípios sobre qualidade de software: modelo MPS.BR e CMMI-DEV versão 1.3. 											'138 - FCC - Governança de TI - MPS.BR - CMMI'
+*8%  Princípios sobre qualidade de software: modelo MPS.BR e CMMI-DEV versão 1.3. 											'138 - FCC - Governança de TI - MPS.BR - CMMI'
 7%  Processos de software. 																									'26  - FCC - Governança de TI - ISO-IEC 12207 '
 7%  Estruturas de dados e de controle de fluxo. 																			'27  - FCC - Algoritmos e Estrutura de Dados - Estrutura de dados - Estruturas de controle e repetição'
 7%  Conceitos sobre orientação a objetos (classes, atributos, objetos, métodos, herança, interfaces e outras hierarquias).  '118 - FCC - Engenharia de Software - Orientação a Objetos'
 5%  Engenharia de requisitos. 																								'67  - FCC - Engenharia de Software - Engenharia de Requisitos'
 'PARE AQUI - 71% CONTEUDO - TERMINE TODOS OS EXERCICIOS DE CIMA - LER MATERIAL COMPLEMENTAR'
-4%  Métodos ágeis (eXtreme Programming – XP, Scrum, Kanban).
-3%  Javascript (ECMAScript 6), bootstrap. jquery.
-3%  Modelagem de processos (BPMN).
-3%  Conceitos de Web Services SOAP e REST.
+4%  Métodos ágeis (eXtreme Programming – XP, Scrum, Kanban). 																'94  - FCC - Engenharia de Software - Processos de Software - Desenvolvimento Ágil'
+3%  Javascript (ECMAScript 6), bootstrap. jquery. 																			'121 - FCC'javascript jquery bootstrap	
+*3%  Modelagem de processos (BPMN). 																						'24  - FCC - Modelagem de Processos de Negócio (BPM) - BPMN (Bussines Process Modeling Notation)'
+3%  Conceitos de Web Services SOAP e REST. 																					'49  - FCC - Arquitetura de Software - WebServices - SOAP (Simple Object Access Protocol)'
 2%  Ciclo de vida de sistemas.
-2%  CSS3. 
-2%  Modelagem funcional.
+2%  CSS3.  																													'40  - FCC - Programação - CSS (Cascading Style Sheets)'
+2%  Modelagem funcional. 																									'67  - FCC - Engenharia de Software - Engenharia de Requisitos'
 'PARE AQUI - 90% CONTEUDO - TERMINE TODOS OS EXERCICIOS DE CIMA - LER MATERIAL COMPLEMENTAR'
-2%  Padrões de Projeto (design patterns).
-1%  Princípios de engenharia de software.
+2%  Padrões de Projeto (design patterns). 																					'34  - FCC - Arquitetura de Software - Padrões de projeto (Design Patterns)'
+1%  Princípios de engenharia de software. 																					'5   - FCC - Engenharia de Software - Conceitos Básicos'
 1%  Modelos de desenvolvimento de sistemas.
-1%  Controle de versões com Git e SVN.
-1%  Conceitos de linguagens orientadas a objetos.
-1%  Web Standards, W3C.
-Testes de software.
+1%  Controle de versões com Git e SVN. 																						'27  - FCC - Engenharia de Software - Gerência de Configuração'
+1%  Conceitos de linguagens orientadas a objetos. 																			'39  - FCC - Programação - Conceitos básicos de programação'
+1%  Web Standards, W3C. 																									'2.208 - FCC - Tecnologia da Informação'web padrão arquitetura W3C
+Testes de software. 																										'60  - FCC - Engenharia de Software - Teste de Software'
 Análise e projeto estruturados de sistemas.
-Modelagem de dados.
-Modelagem orientada a objetos.
+*Modelagem de dados.
+*Modelagem orientada a objetos.
 Funções e procedimentos.
-Conceitos de linguagens estruturadas.
-Conceitos sobre desenvolvimento Web e cliente/servidor.
+Conceitos de linguagens estruturadas. 																						'5   - FCC - Programação - Programação estruturada'	
+Conceitos sobre desenvolvimento Web e cliente/servidor. 																	'6   - FCC - Arquitetura de Software - Arquitetura Cliente-Servidor'
 
+//--
 # Sistemas de apoio à decisão '3 PASSADAS SEM COMPLETAR'
-44% Arquitetura OLAP. 																										'31 - FCC - Banco de Dados - OLAP (On-line Analytical Processing)'
-18% Data Mining. 																											'16 - FCC - Banco de Dados - Data Mining'
-14% Data Warehouse. 																										'53 - FCC - Banco de Dados - DW - Data Warehouse'
+*44% Arquitetura OLAP. 																										'31 - FCC - Banco de Dados - OLAP (On-line Analytical Processing)'
+*18% Data Mining. 																											'16 - FCC - Banco de Dados - Data Mining'
+*14% Data Warehouse. 																										'53 - FCC - Banco de Dados - DW - Data Warehouse'
 'PARE AQUI - 76% CONTEUDO - TERMINE TODOS OS EXERCICIOS DE CIMA - LER MATERIAL COMPLEMENTAR'
-13% ERP.
+*13% ERP.																													'4  - FCC - Arquitetura de Software - ERP (Enterprise Resource Planning)'
 'PARE AQUI - 89% CONTEUDO - TERMINE TODOS OS EXERCICIOS DE CIMA - LER MATERIAL COMPLEMENTAR'
-6%  Business Intelligence: Conceitos Princípios e Processos.
-4%  ETL.
+*6%  Business Intelligence: Conceitos Princípios e Processos. 																'11 - FCC - Banco de Dados - BI (Business Intelligence)'
+*4%  ETL. 																													'6  - FCC - Banco de Dados - ETL (Extract Transform Load)'
 
+//--
 # Governança e Gestão de TI '3 PASSADAS SEM COMPLETAR'
-66% Gestão de serviços de TI com ITIL v3 Edição 2011 - Estrutura, objetivos e conceitos gerais. 							'144 - FCC - Governança de TI - ITIL v. 3'
-8%  Governança e Gestão de TI com COBIT 5 - Estrutura, objetivos e conceitos gerais. 										'32  - FCC - Governança de TI - COBIT v. 5.0'
+*3 - 66% Gestão de serviços de TI com ITIL v3 Edição 2011 - Estrutura, objetivos e conceitos gerais. 						'144 - FCC - Governança de TI - ITIL v. 3'
+*3 - 8%  Governança e Gestão de TI com COBIT 5 - Estrutura, objetivos e conceitos gerais. 									'32  - FCC - Governança de TI - COBIT v. 5.0'
 'PARE AQUI - 74% CONTEUDO - TERMINE TODOS OS EXERCICIOS DE CIMA - LER MATERIAL COMPLEMENTAR'
-8%  Planejamento estratégico de TI – PETI.
-6%  Gestão de projetos com PMBOK 5a Edição.
-5%  Gestão de riscos. Programa de gestão de riscos. Processo de gestão de riscos. Métodos de análise de riscos.
+*8%  Planejamento estratégico de TI – PETI. 																				'20  - FCC - Governança de TI - Planejamento Estratégico de TI (PETI)'
+*6%  Gestão de projetos com PMBOK 5a Edição. 																				'164 - FCC - Gerência de Projetos - PMBOK e Processos de Gerenciamento de Projetos'
+*5%  Gestão de riscos. Programa de gestão de riscos. Processo de gestão de riscos. Métodos de análise de riscos. 			'8   - FCC - Gerência de Projetos - Gerenciamento dos riscos'
 'PARE AQUI - 93% CONTEUDO - TERMINE TODOS OS EXERCICIOS DE CIMA - LER MATERIAL COMPLEMENTAR'
-2%  Estrutura organizacional e responsabilidades da TI. Responsabilidade e papéis da TI. Segregação de funções de TI. Controles de segregação de funções.
-1%  Terceirização da TI.
-1%  Alinhamento estratégico entre áreas de TI e negócios. Políticas e procedimentos.
-1%  Gestão de pessoal.
-1%  Gestão de mudanças organizacionais. Processos de definição, implantação e gestão de políticas organizacionais.
-Práticas de gestão da TI.
+*2%  Estrutura organizacional e responsabilidades da TI. Responsabilidade e papéis da TI. Segregação de funções de TI. Controles de segregação de funções.
+																															'14  - FCC - Governança de TI - Planejamento e Gestão de Sistemas de TI'
+*1%  Terceirização da TI. 																									'22  - FCC - Governança de TI - Conceitos Básicos'
+*1%  Alinhamento estratégico entre áreas de TI e negócios. Políticas e procedimentos. 										'1   - FCC - Governança de TI - Cultura Organizacional'
+*1%  Gestão de pessoal. 																									'41  - FCC - Gerência de Projetos - Áreas de conhecimento'
+*1%  Gestão de mudanças organizacionais. Processos de definição, implantação e gestão de políticas organizacionais. 		'36  - FCC - Gerência de Projetos - Conceitos básicos no Gerenciamento de Projetos'
+*Práticas de gestão da TI. 																									'27  - FCC - Gerência de Projetos - Gerenciamento de Projetos, Programas e Portfólio'
 
+
+//-- --//
 ## ESTRATEGIA DE RESOLUÇÂO
-- NÃO DÁ PRA ESTUDAR TUDO, FAZ EXERCICIO
-- SE CERTEZA
-	PASSA 
-  SENAO
-  	ANOTA E PESQUISA
-- VAI DAR TUDO CERTO
-- ESTUDAR NO ÔNIBUS SÓ OS CONTEUDOS QUE VOCÊ NÃO TEM IDEIA!!!!
-- FAZENDO EXERCICIO VOCE VAI SABER QUAIS NÃO TEM IDEIA!!!!	
+- LER NO ONIBUS CONTEUDOS ATE 90% COM MAIS DE 100 QUESTOES
+- ESTUDAR NA BCE CONTEUDOS ATE 90% COM MAIS DE 100 QUESTOES, AO TERMINAR, FAZER MUITAS QUESTÕES
