8dae0aa2d87c6100a59c55b4403640ed0ac99685
gabrielmirandat <gabrielmirandatt@gmail.com>
2017-09-13 19:56:19 -0300

    Updated pa

---
+++ college/classes/computer_science/algorithms_project_analisys/t2/exercise_paa.cc
@@ -0,0 +1,14 @@
+LOG, SERIES e INDUÇÃO
+9,10,48,49,50,51,52 - discursiva sem opção
+53,54,55,56,57,58,59,60 - só uma opção
+
+Notacao Assintotica
+12,17,22,30,39,40,42,44,48,52,- imagem não abre
+
+Métodos da Iteração
+10,21,22,25,26,27,29,31,32,34,37,38,43,44,45,47,48,49,54, - imagem não abre
+20 - Escrevi Falso e tomei errado e errado sem ponto
+35 - foi pedida na verdade a segunda iteração
+39,40 - deu 2/4
+52 - o que é o que?
+56,57,58,59,60 - discursiva com problema
