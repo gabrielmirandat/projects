fcb86873fabd0d1aac8fddc341d40663be822200
Gabriel Miranda <gabrielmirandatt@gmail.com>
2017-11-01 10:03:48 -0200

    Estudo para serviço usando DDD e Spring

---
+++ college/internship/tcu/tutorials/ddd_servico_representacao.cc
@@ -0,0 +1,96 @@
+// tecnologias
+gradle
+groovy
+spring X JEE
+springboot
+hibernate
+flyway
+spock
+junit
+rxjava
+
+
+// @ (anotation)
+@Deprecated 'não deve ser mais usado'
+@Override 'metodo sobrescrito'
+@Test 'método é teste no JUnit'
+
+@Entity 'declara uma entidade do tipo relacional, como uma tabela, porém é uma classe java'
+@Id 'define o identificador da tabela'
+@SequenceGenerator 'forma de auto incremento de determinado campo'
+
+@SpringBootApplication 'equivalente a @Configuration, @EnableAutoConfiguration e @ComponentScan com valores default'
+@EnableFeignClients
+@Bean 'bean do Spring'
+@GetMapping 'mapeia requisições HTTP GET em métodos específicos'
+@RestController
+@Autowired
+@Value
+@PostMapping
+@RequestBody
+@RequestParam
+@PathVariable
+
+// código
+java.util.logging.Logger // mostra dados na tela
+
+
+// principal
+aplicacao/ // camada da aplicacao
+	build/ // arquivos class da compilacao
+	build.gradle // arquivo de configs de compilacao e outros
+	src/
+		test/ // arquivos de teste da main/
+		main/
+			resources/ // arquivos do banco de dados e controle de versao do flyway
+			groovy/
+				aplicacao/
+				config/
+				infra/
+				rest/
+					AutenticacaoRest.groovy // faz autenticação de usuário
+					// @RestController
+					// @Autowired
+					// @Value
+					// @PostMapping
+					// @RequestBody
+					AutuarRepresentacaoRest.groovy // entender depois
+					// @RestController
+					// @GetMapping
+					// @RequestParam
+					// @PathVariable
+					// @PostMapping
+					// @RequestBody
+					ControladorExcecao.groovy
+					CriaContratoRest.groovy
+					CriarLicitacaoRest.groovy
+					DocumentoRest.groovy
+					PessoaQualificadaRest.groovy
+					ProcessoRest.groovy
+					RepresentacaoRest.groovy
+				sanityrest/
+					Http2ServiceRest.groovy  // controlador REST / mapeia "/", "/entity-flow", "/push"
+					// @RestController
+					// @GetMapping
+				RepresentacaoMain.groovy // roda aplicacao SpringBoot / cria um Bean
+				// @SpringBootApplication
+				// @EnableFeignClients
+				// @Bean
+
+
+
+
+
+dominio/ // camada de dominios
+	build/ // arquivos class da compilacao
+	src/
+	build.gradle // arquivo de configs de compilacao
+
+
+// outros
+gradle/ // arq de propriedades do GRADLE WRAPPER
+k8s/ // arq de config de deploy e outros do RANCHER
+representacao/ // arq de propriedades do SISTEMA REPRESENTACAO
+
+// fora do projeto
+External Libraries/ // bibliotecas baixadas de dependencias
